664fafbfb398cf818a858c4d32b9655c11ea1c4b
bubkoo <bubkoo.wy@gmail.com>
2019-11-14 14:06:07 +0800

    细化 Style 类型定义

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
+import {
+  RoutingFunction,
+  PerimeterFunction,
+} from './core/registry'
 import { FontStyle } from './struct'
-import { PerimeterFunction, RoutingFunction } from './core/registry'
 
 export type Dialect = 'svg' | 'html'
 export type Align = 'left' | 'center' | 'right'
@@ -16,15 +19,116 @@ export interface Size {
   height: number
 }
 
-export interface Style {
-  /**
-   * Custom styles
-   */
-  [key: string]: any
+interface ShapeStyle {
+  shape?: string
+  className?: string
+  pointerEvents?: boolean
 
-  shape?: string | null
+  cursor?: string
+  opacity?: number
+  rotation?: number
+
+  fill?: string
+  fillOpacity?: number
+  gradientColor?: string
+  gradientDirection?: Direction
+
+  stroke?: string
+  strokeWidth?: number
+  strokeOpacity?: number
+  dashed?: boolean
+  dashPattern?: string
+  fixDash?: boolean
+
+  flipH?: boolean
+  flipV?: boolean
+  glass?: boolean
+  shadow?: boolean
+
+  /**
+   * This specifies if the aspect ratio of the cell will be
+   * maintained when resizing.
+   */
+  aspect?: boolean
+
+  /**
+   * This style specifies if a only the background of a cell
+   * should be painted when it is highlighted.
+   */
+  backgroundOutline?: boolean
+}
+
+interface LabelStyle {
+  /**
+   * If this is true then no label is visible for a given cell.
+   */
+  noLabel?: boolean
+  htmlLabel?: boolean
+  horizontal?: boolean
+  fontSize?: number
+  fontColor?: string
+  fontStyle?: FontStyle
+  fontFamily?: string
+  textOpacity?: number
+  textDirection?: WritingDirection
+  align?: Align
+  verticalAlign?: VAlign
+  overflow?: 'fill' | 'width' | 'visible' | 'hidden'
+  whiteSpace?: 'nowrap' | 'wrap'
+  labelBackgroundColor?: string
+  labelBorderColor?: string
+  labelClassName?: string
+  labelWidth?: number
+  labelPadding?: number
+  spacing?: number
+  spacingTop?: number
+  spacingRight?: number
+  spacingBottom?: number
+  spacingLeft?: number
+
+  /**
+   * The horizontal label position of vertices. Possible values are
+   * `left`, `center`, `right`.
+   *
+   * Default is `center`.
+   *
+   * `left` means the entire label bounds is placed completely just
+   * to the left of the node, `right` means adjust to the right and
+   * `center` means the label bounds are vertically aligned with the
+   * bounds of the node.
+   *
+   * Note this style doesn't affect the positioning of label within the
+   * label bounds, to move the label horizontally within the label bounds,
+   * use `align` style.
+   */
+  labelPosition?: LabelPosition
+
+  /**
+   * The vertical label position of vertices. Possible values are
+   * `top`, `middle`, `bottom`.
+   *
+   * Default is `middle`.
+   *
+   * `top` means the entire label bounds is placed completely just
+   * on the top of the node, `bottom` means adjust on the bottom
+   * and `middle` means the label bounds are horizontally aligned
+   * with the bounds of the vertex.
+   *
+   * Note this value doesn't affect the positioning of label within the
+   * label bounds, to move the label vertically within the label bounds,
+   * use `verticalAlign` style.
+   */
+  labelVerticalPosition?: LabelVerticalPosition
+}
+
+interface EdgeStyle {
   edge?: string | RoutingFunction | null
   perimeter?: string | PerimeterFunction | null
+
+  /**
+   * If this is true then no edge style is applied for a given edge.
+   */
+  noEdgeStyle?: boolean
 
   sourcePort?: string
   targetPort?: string
@@ -33,12 +137,9 @@ export interface Style {
   sourcePortConstraint?: string
   targetPortConstraint?: string
 
-  /**
-   * Defines if the connection points on either end of the edge should be
-   * computed so that the edge is vertical or horizontal if possible and
-   * if the point is not at a fixed location.
-   */
-  orthogonal?: boolean
+  perimeterSpacing?: number
+  sourcePerimeterSpacing?: number
+  targetPerimeterSpacing?: number
 
   /**
    * The horizontal relative coordinate connection point of an edge with
@@ -104,89 +205,30 @@ export interface Style {
    */
   entryPerimeter?: boolean
 
-  cursor?: string
-  opacity?: number
-  rotation?: number
-  className?: string
-  pointerEvents?: boolean
-
-  fill?: string
-  fillOpacity?: number
-  gradientColor?: string
-  gradientDirection?: Direction
-
-  stroke?: string
-  strokeWidth?: number
-  strokeOpacity?: number
-  dashed?: boolean
-  dashPattern?: string
-  fixDash?: boolean
-
-  flipH?: boolean
-  flipV?: boolean
+  /**
+   * Defines if the connection points on either end of the edge should be
+   * computed so that the edge is vertical or horizontal if possible and
+   * if the point is not at a fixed location.
+   */
+  orthogonal?: boolean
 
   /**
-   * If this is true then no label is visible for a given cell.
+   * This style defines how the three segment orthogonal edge style
+   * leaves its terminal vertices. The vertical style leaves the
+   * terminal vertices at the top and bottom sides.
    */
-  noLabel?: boolean
-  fontSize?: number
-  fontColor?: string
-  fontStyle?: FontStyle
-  fontFamily?: string
-  textOpacity?: number
-  textDirection?: WritingDirection
-  align?: Align
-  verticalAlign?: VAlign
-  overflow?: 'fill' | 'width' | 'visible' | 'hidden'
-  whiteSpace?: 'nowrap' | 'wrap'
-  htmlLabel?: boolean
-  horizontal?: boolean
-  labelBackgroundColor?: string
-  labelBorderColor?: string
-  labelClassName?: string
-  labelWidth?: number
-  labelPadding?: number
+  elbow?: 'horizontal' | 'vertical'
 
-  /**
-   * The horizontal label position of vertices. Possible values are
-   * `left`, `center`, `right`.
-   *
-   * Default is `center`.
-   *
-   * `left` means the entire label bounds is placed completely just
-   * to the left of the node, `right` means adjust to the right and
-   * `center` means the label bounds are vertically aligned with the
-   * bounds of the node.
-   *
-   * Note this style doesn't affect the positioning of label within the
-   * label bounds, to move the label horizontally within the label bounds,
-   * use `align` style.
-   */
-  labelPosition?: LabelPosition
+  segment?: number
+  endSize?: number
+  endArrow?: string
+  endFilled?: boolean
+  startSize?: number
+  startArrow?: string
+  startFilled?: boolean
+}
 
-  /**
-   * The vertical label position of vertices. Possible values are
-   * `top`, `middle`, `bottom`.
-   *
-   * Default is `middle`.
-   *
-   * `top` means the entire label bounds is placed completely just
-   * on the top of the node, `bottom` means adjust on the bottom
-   * and `middle` means the label bounds are horizontally aligned
-   * with the bounds of the vertex.
-   *
-   * Note this value doesn't affect the positioning of label within the
-   * label bounds, to move the label vertically within the label bounds,
-   * use `verticalAlign` style.
-   */
-  labelVerticalPosition?: LabelVerticalPosition
-
-  direction?: Direction
-
-  separatorColor?: string
-  swimlaneFillColor?: string
-  margin?: number
-
+interface ImageStyle {
   image?: string
   imageWidth?: number
   imageHeight?: number
@@ -194,18 +236,15 @@ export interface Style {
   imageBorderColor?: string
   imageAlign?: Align
   imageVerticalAlign?: VAlign
+  imageFlipH?: boolean
+  imageFlipV?: boolean
   /**
    * Specify whether keep image aspect or not.
    */
   imageAspect?: boolean
-  imageFlipH?: boolean
-  imageFlipV?: boolean
+}
 
-  /**
-   * If this is true then no edge style is applied for a given edge.
-   */
-  noEdgeStyle?: boolean
-
+interface IndicatorStyle {
   indicatorShape?: string
   indicatorImage?: string
   indicatorColor?: string
@@ -215,61 +254,48 @@ export interface Style {
   indicatorWidth?: number
   indicatorHeight?: number
   indicatorDirection?: Direction
+}
 
-  segment?: number
-  endSize?: number
-  endArrow?: string
-  endFilled?: boolean
-  startSize?: number
-  startArrow?: string
-  startFilled?: boolean
+interface CustomStyle {
+  [key: string]: any
+}
 
-  glass?: boolean
-  shadow?: boolean
-  rounded?: boolean
-  curved?: boolean
-  arcSize?: number
-  absoluteArcSize?: boolean
+interface CustomCSS {
+  css?: {
+    [selector: string]: Partial<CSSStyleDeclaration>,
+  }
+}
 
-  perimeterSpacing?: number
-  sourcePerimeterSpacing?: number
-  targetPerimeterSpacing?: number
-  spacing?: number
-  spacingTop?: number
-  spacingRight?: number
-  spacingBottom?: number
-  spacingLeft?: number
+export interface Style extends
+  ShapeStyle,
+  LabelStyle,
+  EdgeStyle,
+  ImageStyle,
+  IndicatorStyle,
+  CustomStyle,
+  CustomCSS {
+
+  direction?: Direction
 
   /**
    * This style specifies whether the line between the title
    * regio of a swimlane should be visible.
    */
   swimlaneLine?: boolean
+  swimlaneFillColor?: string
+  separatorColor?: string
+  margin?: number
+
+  rounded?: boolean
+  curved?: boolean
+  arcSize?: number
+  absoluteArcSize?: boolean
 
   /**
    * This style defines if the direction style should be taken into
    * account when computing the fixed point location for connected edges.
    */
   anchorPointDirection?: boolean
-
-  /**
-   * This style specifies if a only the background of a cell
-   * should be painted when it is highlighted.
-   */
-  backgroundOutline?: boolean
-
-  /**
-   * This style defines how the three segment orthogonal edge style
-   * leaves its terminal vertices. The vertical style leaves the
-   * terminal vertices at the top and bottom sides.
-   */
-  elbow?: 'horizontal' | 'vertical'
-
-  /**
-   * This specifies if the aspect ratio of the cell will be
-   * maintained when resizing.
-   */
-  aspect?: boolean
 
   locked?: boolean
 
@@ -352,8 +378,4 @@ export interface Style {
   orthogonalLoop?: boolean
   routingCenterX?: number
   routingCenterY?: number
-
-  css?: {
-    [selector: string]: Partial<CSSStyleDeclaration>,
-  }
 }
