212fb333da57a0e5211970b4f6897dc241db710d
bubkoo <bubkoo.wy@gmail.com>
2019-11-19 10:15:09 +0800

    update mousetrap type defines

--- src/typings/mousetrap.d.ts
+++ src/typings/mousetrap.d.ts
@@ -2,37 +2,27 @@ interface ExtendedKeyboardEvent extends KeyboardEvent {
   returnValue: boolean // IE returnValue
 }
 
+type Action = 'keypress' | 'keydown' | 'keyup'
+type Callback = (e: ExtendedKeyboardEvent, combo: string) => any
+
 interface MousetrapStatic {
   (el: Element): MousetrapInstance
   new(el?: Element): MousetrapInstance
   reset(): MousetrapInstance
   addKeycodes(keycodes: { [key: number]: string }): void
   stopCallback: (e: ExtendedKeyboardEvent, element: Element, combo: string) => boolean
-  bind(
-    keys: string | string[],
-    callback: (e: ExtendedKeyboardEvent, combo: string) => any,
-    action?: string,
-  ): MousetrapInstance
+  bind(keys: string | string[], callback: Callback, action?: Action): MousetrapInstance
   unbind(keys: string | string[], action?: string): MousetrapInstance
   trigger(keys: string, action?: string): MousetrapInstance
-
-  bindGlobal(
-    keyArray: string | string[],
-    callback: (e: ExtendedKeyboardEvent, combo: string) => any,
-    action?: string,
-  ): void
+  bindGlobal(keyArray: string | string[], callback: Callback, action?: Action): void
 }
 
 interface MousetrapInstance {
   reset(): this
   stopCallback(e: ExtendedKeyboardEvent, element: Element, combo: string): boolean
-  bind(
-    keys: string | string[],
-    callback: (e: ExtendedKeyboardEvent, combo: string) => any,
-    action?: string,
-  ): this
-  unbind(keys: string | string[], action?: string): this
-  trigger(keys: string, action?: string): this
+  bind(keys: string | string[], callback: Callback, action?: Action): this
+  unbind(keys: string | string[], action?: Action): this
+  trigger(keys: string, action?: Action): this
   handleKey(character: string, modifiers: string[], e: ExtendedKeyboardEvent): void
 }
 
