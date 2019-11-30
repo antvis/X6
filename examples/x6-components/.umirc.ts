9898a1d591375c54b49ec7a3fb6d88a776b996eb
bubkoo <bubkoo.wy@gmail.com>
2019-11-30 18:48:39 +0800

    examples for x6-components

---
+++ examples/x6-components/.umirc.ts
@@ -0,0 +1,39 @@
+import { IConfig } from "umi-types";
+
+// ref: https://umijs.org/config/
+const config: IConfig = {
+  treeShaking: true,
+  /**
+   * 调试时直接引用了 packages/x6-components 中的样式文件，
+   * 所以需要禁用 css modules
+   */
+  disableCSSModules: true,
+  plugins: [
+    [
+      "umi-plugin-react",
+      {
+        antd: true,
+        dva: false,
+        dynamicImport: false,
+        title: "x6 Components",
+        dll: false,
+        routes: {
+          exclude: [/components\//]
+        }
+      }
+    ]
+  ],
+  extraBabelPlugins: [
+    [
+      "import",
+      {
+        libraryName: "@antv/x6-components",
+        libraryDirectory: "es",
+        transformToDefaultImport: false,
+        style: true
+      }
+    ]
+  ]
+};
+
+export default config;
---
+++ examples/x6-components/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function() {
+  return (
+    <div>
+      <h1>React components for building x6 editors</h1>
+    </div>
+  );
+}
