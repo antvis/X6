ca10ca5cac29b397cf897cb42aee337e91ad4602
bubkoo <bubkoo.wy@gmail.com>
2019-12-17 21:06:58 +0800

    refactor: remove unused detections

--- packages/x6/src/common/detector.ts
+++ packages/x6/src/common/detector.ts
@@ -1,20 +1,9 @@
 const ua = navigator.userAgent
 
 export namespace detector {
-  /**
-   * A flag indicating whether the platform is Windiws.
-   */
-  export const IS_WINDOWS = navigator.appVersion.indexOf('Win') > 0
-
-  /**
-   * A flag indicating whether the platform is Mac.
-   */
   export const IS_MAC = navigator.appVersion.indexOf('Mac') > 0
-
-  /**
-   * A flag indicating whether the platform is iPad, iPhone or iPod.
-   */
   export const IS_IOS = ua.match(/(iPad|iPhone|iPod)/g) ? true : false
+  export const IS_WINDOWS = navigator.appVersion.indexOf('Win') > 0
 
   export const IS_IE = ua.indexOf('MSIE') >= 0
   export const IS_IE11 = !!ua.match(/Trident\/7\./)
@@ -29,24 +18,6 @@ export namespace detector {
     ua.indexOf('Edge/') < 0
 
   /**
-   * A flag indicating whether the the current browser is Opera.
-   */
-  export const IS_OPERA = ua.indexOf('Opera/') >= 0 || ua.indexOf('OPR/') >= 0
-
-  /**
-   * A flag indicating whether the the current browser is Safari.
-   */
-  export const IS_SAFARI =
-    ua.indexOf('AppleWebKit/') >= 0 &&
-    ua.indexOf('Chrome/') < 0 &&
-    ua.indexOf('Edge/') < 0
-
-  /**
-   * A flag indicating whether the the current browser is Google Chrome.
-   */
-  export const IS_CHROME = ua.indexOf('Chrome/') >= 0 && ua.indexOf('Edge/') < 0
-
-  /**
    * A flag indicating whether the the this is running inside a Chrome App.
    */
   export const IS_CHROME_APP =
@@ -54,38 +25,13 @@ export namespace detector {
     (window as any).chrome.app != null &&
     (window as any).chrome.app.runtime != null
 
-  /**
-   * A flag indicating whether the the current browser is Firefox.
-   */
+  export const IS_CHROME = ua.indexOf('Chrome/') >= 0 && ua.indexOf('Edge/') < 0
+  export const IS_OPERA = ua.indexOf('Opera/') >= 0 || ua.indexOf('OPR/') >= 0
   export const IS_FIREFOX = ua.indexOf('Firefox/') >= 0
-
-  /**
-   * A flag indicating whether `-moz-transform` is available as a CSS style.
-   * This is the case for all Firefox-based browsers newer than or equal 3,
-   * such as Camino, Iceweasel, Seamonkey and Iceape.
-   */
-  export const IS_MT =
-    (ua.indexOf('Firefox/') >= 0 &&
-      ua.indexOf('Firefox/1.') < 0 &&
-      ua.indexOf('Firefox/2.') < 0) ||
-    (ua.indexOf('Iceweasel/') >= 0 &&
-      ua.indexOf('Iceweasel/1.') < 0 &&
-      ua.indexOf('Iceweasel/2.') < 0) ||
-    (ua.indexOf('SeaMonkey/') >= 0 && ua.indexOf('SeaMonkey/1.') < 0) ||
-    (ua.indexOf('Iceape/') >= 0 && ua.indexOf('Iceape/1.') < 0)
-
-  /**
-   * A flag indicating whether `-o-transform` is available as a CSS style,
-   * ie for Opera browsers based on a Presto engine with version 2.5 or later.
-   */
-  export const IS_OT =
-    ua.indexOf('Presto/') >= 0 &&
-    ua.indexOf('Presto/2.4.') < 0 &&
-    ua.indexOf('Presto/2.3.') < 0 &&
-    ua.indexOf('Presto/2.2.') < 0 &&
-    ua.indexOf('Presto/2.1.') < 0 &&
-    ua.indexOf('Presto/2.0.') < 0 &&
-    ua.indexOf('Presto/1.') < 0
+  export const IS_SAFARI =
+    ua.indexOf('AppleWebKit/') >= 0 &&
+    ua.indexOf('Chrome/') < 0 &&
+    ua.indexOf('Edge/') < 0
 
   /**
    * A flag indicating whether this device supports touchstart/-move/-end
