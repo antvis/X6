5204898edcec92e4897a0767ff1f44c4108f4ab0
bubkoo <bubkoo.wy@gmail.com>
2019-12-19 19:43:45 +0800

    fix: usage of graph.render

--- examples/x6-example-features/src/pages/helloworld.tsx
+++ examples/x6-example-features/src/pages/helloworld.tsx
@@ -8,22 +8,33 @@ export default class Example extends React.Component {
   componentDidMount() {
     const graph = new Graph(this.container)
 
-    graph.batchUpdate(() => {
-      const node1 = graph.addNode({
-        x: 60,
-        y: 60,
-        width: 80,
-        height: 30,
-        label: 'Hello',
-      })
-      const node2 = graph.addNode({
-        x: 240,
-        y: 240,
-        width: 80,
-        height: 30,
-        label: 'World',
-      })
-      graph.addEdge({ label: 'Edge Label', source: node1, target: node2 })
+    graph.render({
+      nodes: [
+        {
+          id: 'node-0',
+          x: 60,
+          y: 60,
+          width: 80,
+          height: 30,
+          label: 'Hello',
+        },
+        {
+          id: 'node-1',
+          x: 240,
+          y: 240,
+          width: 80,
+          height: 30,
+          label: 'World',
+        },
+      ],
+      edges: [
+        {
+          id: 'edge-0',
+          source: 'node-0',
+          target: 'node-1',
+          label: 'Edge Label',
+        },
+      ],
     })
   }
 
