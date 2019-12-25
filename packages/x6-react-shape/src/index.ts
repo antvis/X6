527e5010e422dbfb0f15c8edc4d4c358bc54772b
bubkoo <bubkoo.wy@gmail.com>
2019-12-25 16:29:40 +0800

    feat: only export the shape

--- packages/x6-react-shape/src/index.ts
+++ packages/x6-react-shape/src/index.ts
@@ -1,9 +1,3 @@
-export * from './shape'
-
-import { Shape } from '@antv/x6'
-import { ReactShape } from './shape'
 import './extend'
 
-export function register(name: string = 'react', force: boolean = false) {
-  Shape.register(name, ReactShape, force)
-}
+export * from './shape'
