839c931639422eeb220ba77a3b580fa1a3f8e0e1
bubkoo <bubkoo.wy@gmail.com>
2019-12-25 16:15:27 +0800

    refactor: file rename

--- packages/x6-react-shape/src/index.ts
+++ packages/x6-react-shape/src/index.ts
@@ -2,7 +2,7 @@ export * from './shape'
 
 import { Shape } from '@antv/x6'
 import { ReactShape } from './shape'
-import './hack'
+import './extend'
 
 export function register(name: string = 'react', force: boolean = false) {
   Shape.register(name, ReactShape, force)
