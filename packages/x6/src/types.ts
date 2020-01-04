2ed89e832a1d99b9d8a4e45d199858a7866fd70a
崖崖崖 <bubkoo.wy@gmail.com>
2020-01-05 03:07:25 +0800

    feat: use utility-types

--- packages/x6/src/types.ts
+++ packages/x6/src/types.ts
@@ -2,6 +2,27 @@ import { Route } from './route'
 import { FontStyle } from './enum'
 import { Perimeter } from './perimeter'
 
+export type JSONPrimitive = boolean | number | string | null
+
+export type JSONValue = JSONPrimitive | JSONObject | JSONArray
+
+export interface JSONObject {
+  [key: string]: JSONValue
+}
+
+export interface JSONArray extends Array<JSONValue> {}
+
+export interface ReadonlyJSONObject {
+  readonly [key: string]: ReadonlyJSONValue
+}
+
+export interface ReadonlyJSONArray extends ReadonlyArray<ReadonlyJSONValue> {}
+
+export type ReadonlyJSONValue =
+  | JSONPrimitive
+  | ReadonlyJSONObject
+  | ReadonlyJSONArray
+
 export type Dialect = 'svg' | 'html'
 export type Align = 'left' | 'center' | 'right'
 export type VAlign = 'top' | 'middle' | 'bottom'
