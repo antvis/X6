e2319a90103851ff90e21e2011e2b7ad16ff861c
崖崖崖 <bubkoo.wy@gmail.com>
2020-01-06 14:44:37 +0800

    chore: build after bootstrap

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
 lerna run build --scope @antv/x6-components &
-lerna run build --scope @antv/x6-util
-await
-
-lerna run build --scope @antv/x6-dom-util &
-lerna run build --scope @antv/x6-dom-event &
-lerna run build --scope @antv/x6-event-emitter &
-lerna run build --scope @antv/x6-geometry
-await
-
 lerna run build --scope @antv/x6
+
+await
+
 lerna run build --scope @antv/x6-react-shape
 
 
