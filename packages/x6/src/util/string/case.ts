71db3681602c45efd8c2e64487b8581f14454ec6
崖崖崖 <bubkoo.wy@gmail.com>
2020-02-25 22:33:31 +0800

    feat: case convert methods

---
+++ packages/x6/src/util/string/case.ts
@@ -0,0 +1,39 @@
+export {
+  camelCase,
+  kebabCase,
+  startCase,
+  snakeCase,
+  lowerCase,
+  upperCase,
+  lowerFirst,
+  upperFirst,
+  capitalize,
+} from 'lodash'
+
+import { camelCase, startCase, upperCase, lowerCase, upperFirst } from 'lodash'
+
+// @see: https://medium.com/@robertsavian/javascript-case-converters-using-lodash-4f2f964091cc
+
+export function pascalCase(str?: string) {
+  return startCase(camelCase(str)).replace(/ /g, '')
+}
+
+export function constantCase(str?: string) {
+  return upperCase(str).replace(/ /g, '_')
+}
+
+export function dotCase(str?: string) {
+  return lowerCase(str).replace(/ /g, '.')
+}
+
+export function pathCase(str?: string) {
+  return lowerCase(str).replace(/ /g, '/')
+}
+
+export function sentenceCase(str?: string) {
+  return upperFirst(lowerCase(str))
+}
+
+export function titleCase(str?: string) {
+  return startCase(camelCase(str))
+}
