1dae65f63bdc31a7904bfa993a963e3d08ebb13d
崖崖崖 <bubkoo.wy@gmail.com>
2020-03-31 11:28:22 +0800

    feat: grid registry

---
+++ packages/x6/src/research/grid/double-mesh.ts
@@ -0,0 +1,57 @@
+import { v } from '../../v'
+import { Grid } from './index'
+
+export interface DoubleMeshOptions extends Grid.Options {
+  factor?: number
+}
+
+export const doubleMesh: Grid.Definition<DoubleMeshOptions>[] = [
+  {
+    color: '#aaaaaa',
+    thickness: 1,
+    markup: 'path',
+    update(elem, options) {
+      let d
+      const width = options.width
+      const height = options.height
+      const thickness = options.thickness
+
+      if (width - thickness >= 0 && height - thickness >= 0) {
+        d = ['M', width, 0, 'H0 M0 0 V0', height].join(' ')
+      } else {
+        d = 'M 0 0 0 0'
+      }
+
+      v.attr(elem, {
+        d,
+        stroke: options.color,
+        'stroke-width': options.thickness,
+      })
+    },
+  },
+  {
+    color: '#000000',
+    thickness: 3,
+    factor: 4,
+    markup: 'path',
+    update(elem, options) {
+      let d
+      const factor = options.factor || 1
+      const width = options.width * factor
+      const height = options.height * factor
+      const thickness = options.thickness
+
+      if (width - thickness >= 0 && height - thickness >= 0) {
+        d = ['M', width, 0, 'H0 M0 0 V0', height].join(' ')
+      } else {
+        d = 'M 0 0 0 0'
+      }
+
+      v.attr(elem, {
+        d,
+        stroke: options.color,
+        'stroke-width': options.thickness,
+      })
+    },
+  },
+]
---
+++ packages/x6/src/research/grid/fixed-dot.ts
@@ -0,0 +1,21 @@
+import { v } from '../../v'
+import { Grid } from './index'
+
+export interface FixedDotOptions extends Grid.Options {}
+
+export const fixedDot: Grid.Definition<FixedDotOptions> = {
+  color: '#aaaaaa',
+  thickness: 1,
+  markup: 'rect',
+  update(elem, options) {
+    const size =
+      options.sx <= 1 ? options.thickness * options.sx : options.thickness
+    v.attr(elem, {
+      width: size,
+      height: size,
+      rx: size,
+      ry: size,
+      fill: options.color,
+    })
+  },
+}
