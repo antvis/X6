7d5c95d995620bbf9995a76d98ee6f582dba704d
崖崖崖 <bubkoo.wy@gmail.com>
2020-04-18 10:44:48 +0800

    feat: debounce

--- packages/x6/src/util/function/function.ts
+++ packages/x6/src/util/function/function.ts
@@ -1,4 +1,4 @@
-export { isFunction, once, noop } from 'lodash'
+export { isFunction, once, noop, debounce } from 'lodash'
 
 export function invoke<T>(
   func: ((...args: any[]) => T) | null | undefined,
