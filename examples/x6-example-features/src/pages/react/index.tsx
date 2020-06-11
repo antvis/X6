3e924615950a3d382b8587d8f664428bff0ab7fe
崖崖崖 <bubkoo.wy@gmail.com>
2020-06-11 13:46:30 +0800

    test: test point static mathod

--- examples/x6-example-features/src/pages/react/index.tsx
+++ examples/x6-example-features/src/pages/react/index.tsx
@@ -69,6 +69,8 @@ export default class Example extends React.Component {
       source,
       target,
     })
+
+    console.log(graph.toJSON())
   }
 
   refContainer = (container: HTMLDivElement) => {
