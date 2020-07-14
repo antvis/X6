aa11f84ad1c2c747148555dfc917620b58d980ab
bubkoo <bubkoo.wy@gmail.com>
2020-07-15 02:11:51 +0800

    fix: type lost after mixin, instanceof failed
    should ignore "constructor" when mixin

--- packages/x6/src/util/object/mixins.ts
+++ packages/x6/src/util/object/mixins.ts
@@ -2,13 +2,15 @@
  * @see https://www.typescriptlang.org/docs/handbook/mixins.html
  */
 export function applyMixins(derivedCtor: any, ...baseCtors: any[]) {
-  baseCtors.forEach(baseCtor => {
-    Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-      Object.defineProperty(
-        derivedCtor.prototype,
-        name,
-        Object.getOwnPropertyDescriptor(baseCtor.prototype, name)!,
-      )
+  baseCtors.forEach((baseCtor) => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+      if (name !== 'constructor') {
+        Object.defineProperty(
+          derivedCtor.prototype,
+          name,
+          Object.getOwnPropertyDescriptor(baseCtor.prototype, name)!,
+        )
+      }
     })
   })
 }
