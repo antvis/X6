dc3c8c426223f520aebdff95c19b17bc283cbcf2
bubkoo <bubkoo.wy@gmail.com>
2020-07-15 10:49:00 +0800

    feat: strict option for line layout

--- packages/x6/src/registry/port-layout/line.ts
+++ packages/x6/src/registry/port-layout/line.ts
@@ -2,7 +2,9 @@ import { Point, Line } from '../../geometry'
 import { normalizePoint, toResult } from './util'
 import { PortLayout } from './index'
 
-export interface SideArgs extends PortLayout.CommonArgs {}
+export interface SideArgs extends PortLayout.CommonArgs {
+  strict?: boolean
+}
 
 export interface LineArgs extends SideArgs {
   start?: Point.PointLike
@@ -23,58 +25,76 @@ export const line: PortLayout.Definition<LineArgs> = (
     groupPositionArgs.end || elemBBox.getCorner(),
   )
 
-  return lineLayout(portsPositionArgs, start, end)
+  return lineLayout(portsPositionArgs, start, end, groupPositionArgs)
 }
 
 export const left: PortLayout.Definition<SideArgs> = (
   portsPositionArgs,
   elemBBox,
+  groupPositionArgs,
 ) => {
   return lineLayout(
     portsPositionArgs,
     elemBBox.getTopLeft(),
     elemBBox.getBottomLeft(),
+    groupPositionArgs,
   )
 }
 
 export const right: PortLayout.Definition<SideArgs> = (
   portsPositionArgs,
   elemBBox,
+  groupPositionArgs,
 ) => {
   return lineLayout(
     portsPositionArgs,
     elemBBox.getTopRight(),
     elemBBox.getBottomRight(),
+    groupPositionArgs,
   )
 }
 
 export const top: PortLayout.Definition<SideArgs> = (
   portsPositionArgs,
   elemBBox,
+  groupPositionArgs,
 ) => {
   return lineLayout(
     portsPositionArgs,
     elemBBox.getTopLeft(),
     elemBBox.getTopRight(),
+    groupPositionArgs,
   )
 }
 
 export const bottom: PortLayout.Definition<SideArgs> = (
   portsPositionArgs,
   elemBBox,
+  groupPositionArgs,
 ) => {
   return lineLayout(
     portsPositionArgs,
     elemBBox.getBottomLeft(),
     elemBBox.getBottomRight(),
+    groupPositionArgs,
   )
 }
 
-function lineLayout(portsPositionArgs: SideArgs[], p1: Point, p2: Point) {
+function lineLayout(
+  portsPositionArgs: SideArgs[],
+  p1: Point,
+  p2: Point,
+  groupPositionArgs: SideArgs,
+) {
   const line = new Line(p1, p2)
   const length = portsPositionArgs.length
-  return portsPositionArgs.map((offset, index) => {
-    const p = line.pointAt((index + 0.5) / length)
+  return portsPositionArgs.map(({ strict, ...offset }, index) => {
+    const ratio =
+      strict || groupPositionArgs.strict
+        ? (index + 1) / (length + 1)
+        : (index + 0.5) / length
+
+    const p = line.pointAt(ratio)
     if (offset.dx || offset.dy) {
       p.translate(offset.dx || 0, offset.dy || 0)
     }
