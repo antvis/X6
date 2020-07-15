e3558498d570c2c08402d7d7e2494d8068db070c
bubkoo <bubkoo.wy@gmail.com>
2020-07-16 00:34:05 +0800

    refactor: use helper scripts and components

--- examples/x6-example-sites/packages/api/registry/port-layout/ellipse/src/index.tsx
+++ examples/x6-example-sites/packages/api/registry/port-layout/ellipse/src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
+import { Wrap } from '@antv/x6-example-sites-helper'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Wrap>
+    <App />
+  </Wrap>,
+  document.getElementById('root'),
+)
