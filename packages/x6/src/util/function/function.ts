d025222ff1e93c3fd9266ea07328947be5692434
zdc1111 <39116292+zdc1111@users.noreply.github.com>
2020-07-16 17:21:58 +0800

    Update function.ts
    fix: remove redundant code

--- packages/x6/src/util/function/function.ts
+++ packages/x6/src/util/function/function.ts
@@ -4,33 +4,12 @@ export function invoke<T>(
   func: ((...args: any[]) => T) | null | undefined,
   args: any[],
   ctx: any,
-): T {
-  let ret
-
+): T{
   if (func != null && typeof func === 'function') {
     const fn = func as Function
-    const len = args.length
-
-    if (len === 0) {
-      ret = fn.call(ctx)
-    } else if (len === 1) {
-      ret = fn.call(ctx, args[0])
-    } else if (len === 2) {
-      ret = fn.call(ctx, args[0], args[1])
-    } else if (len === 3) {
-      ret = fn.call(ctx, args[0], args[1], args[2])
-    } else if (len === 4) {
-      ret = fn.call(ctx, args[0], args[1], args[2], args[3])
-    } else if (len === 5) {
-      ret = fn.call(ctx, args[0], args[1], args[2], args[3], args[4])
-    } else if (len === 6) {
-      ret = fn.call(ctx, args[0], args[1], args[2], args[3], args[4], args[5])
-    } else {
-      ret = fn.apply(ctx, args)
-    }
+    return fn.apply(ctx, args)
   }
-
-  return ret
+  return undefined as any
 }
 
 export function apply<T>(
