0bf987bc598916146b3fca9a35a1f6ecc98c8cf4
zdc1111 <39116292+zdc1111@users.noreply.github.com>
2020-07-28 23:34:22 +0800

    perf: ⚡️ Optimize apply and call (#203)
    Co-authored-by: 崖崖崖 <bubkoo.wy@gmail.com>

--- packages/x6/src/util/function/function.ts
+++ packages/x6/src/util/function/function.ts
@@ -1,40 +1,40 @@
 export { isFunction, once, noop, debounce, defer } from 'lodash-es'
 
 export function invoke<T>(
-  func: ((...args: any[]) => T) | null | undefined,
+  func: (...args: any[]) => T,
   args: any[],
   ctx: any,
 ): T {
-  let ret
-
-  if (func != null && typeof func === 'function') {
-    const fn = func as Function
-    const len = args.length
-
-    if (len === 0) {
-      ret = fn.call(ctx)
-    } else if (len === 1) {
-      ret = fn.call(ctx, args[0])
-    } else if (len === 2) {
-      ret = fn.call(ctx, args[0], args[1])
-    } else if (len === 3) {
-      ret = fn.call(ctx, args[0], args[1], args[2])
-    } else if (len === 4) {
-      ret = fn.call(ctx, args[0], args[1], args[2], args[3])
-    } else if (len === 5) {
-      ret = fn.call(ctx, args[0], args[1], args[2], args[3], args[4])
-    } else if (len === 6) {
-      ret = fn.call(ctx, args[0], args[1], args[2], args[3], args[4], args[5])
-    } else {
-      ret = fn.apply(ctx, args)
-    }
+  switch (args.length) {
+    case 0:
+      return func.call(ctx)
+    case 1:
+      return func.call(ctx, args[0])
+    case 2:
+      return func.call(ctx, args[0], args[1])
+    case 3:
+      return func.call(ctx, args[0], args[1], args[2])
+    case 4:
+      return func.call(ctx, args[0], args[1], args[2], args[3])
+    case 5:
+      return func.call(ctx, args[0], args[1], args[2], args[3], args[4])
+    case 6:
+      return func.call(
+        ctx,
+        args[0],
+        args[1],
+        args[2],
+        args[3],
+        args[4],
+        args[5],
+      )
+    default:
+      return func.apply(ctx, args)
   }
-
-  return ret
 }
 
 export function apply<T>(
-  func: ((...args: any[]) => T) | null | undefined,
+  func: (...args: any[]) => T,
   ctx: any,
   args: any[] = [],
 ): T {
@@ -42,7 +42,7 @@ export function apply<T>(
 }
 
 export function call<T>(
-  func: ((...args: any[]) => T) | null | undefined,
+  func: (...args: any[]) => T,
   ctx: any,
   ...args: any[]
 ): T {
