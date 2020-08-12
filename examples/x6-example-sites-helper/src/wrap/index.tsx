7b1a0ae9a007c94f50c96f6dc4762dcdcedd16c2
bubkoo <bubkoo.wy@gmail.com>
2020-08-12 23:33:46 +0800

    fix: 🐛 store iframe size

--- examples/x6-example-sites-helper/src/wrap/index.tsx
+++ examples/x6-example-sites-helper/src/wrap/index.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { Toolbar } from '../toolbar'
 import './index.css'
 
-export class Wrap extends React.Component {
+export class Wrap extends React.Component<Wrap.Props, Wrap.State> {
   private container: HTMLDivElement
+
+  constructor(props: Wrap.Props) {
+    super(props)
+    Wrap.restoreIframeSize()
+  }
 
   componentDidMount() {
     this.updateIframeSize()
@@ -27,6 +32,7 @@ export class Wrap extends React.Component {
       iframe.style.height = `${height + 16}px`
       iframe.style.border = '0'
       iframe.style.overflow = 'hidden'
+      Wrap.saveIframeSize()
     }
   }
 
@@ -41,5 +47,51 @@ export class Wrap extends React.Component {
         {this.props.children}
       </div>
     )
+  }
+}
+
+export namespace Wrap {
+  export interface Props {}
+  export interface State {}
+}
+
+export namespace Wrap {
+  const STORE_KEY = window.location.pathname
+  const STORE_ROOT = 'x6-iframe-size'
+
+  function getData() {
+    const raw = localStorage.getItem(STORE_ROOT)
+    let data
+    if (raw) {
+      try {
+        data = JSON.parse(raw)
+      } catch (error) {}
+    } else {
+      data = {}
+    }
+    return data
+  }
+
+  export function saveIframeSize() {
+    const iframe = window.frameElement as HTMLIFrameElement
+    if (iframe) {
+      const style = iframe.style
+      const size = { width: style.width, height: style.height }
+      const data = getData()
+      data[STORE_KEY] = size
+      localStorage.setItem(STORE_ROOT, JSON.parse(data))
+    }
+  }
+
+  export function restoreIframeSize() {
+    const iframe = window.frameElement as HTMLIFrameElement
+    if (iframe) {
+      const data = getData()
+      const size = data[STORE_KEY]
+      if (size) {
+        iframe.style.width = size.width || '100%'
+        iframe.style.height = size.height || 'auto'
+      }
+    }
   }
 }
