3c8cb42afc004cbd331692601cf2a0e41a51ab68
bubkoo <bubkoo.wy@gmail.com>
2020-08-28 16:00:14 +0800

    feat: ✨ set global cache dir

--- examples/x6-example-sites-helper/scripts/speedup.js
+++ examples/x6-example-sites-helper/scripts/speedup.js
@@ -8,17 +8,38 @@ const parts = repo.split('/')
 const index = parts.indexOf('packages')
 const root = index >= 0 ? parts.slice(0, index).join('/') : repo
 const home = path.resolve(root, '../../')
-
 const configDir = 'node_modules/react-scripts/config'
 const configFile = path.join(home, configDir, 'webpack.config.js')
 let content = fs.readFileSync(configFile, { encoding: 'utf8' })
+
+// 修改 node_modules 下 react-scripts 的配置文件来提升 DEMO 的构建速度
 if (content.indexOf('minimize: isEnvProduction') !== -1) {
   content = content
+    // 禁用压缩
     .replace('minimize: isEnvProduction', 'minimize: false')
+    // 禁止生成 manifest
     .replace('new ManifestPlugin({', 'false && new ManifestPlugin({')
+    // 禁止生成 worker
     .replace(
       'new WorkboxWebpackPlugin.GenerateSW({',
-      'false && new WorkboxWebpackPlugin.GenerateSW({',
+      `false && new WorkboxWebpackPlugin.GenerateSW({`,
+    )
+    // 修改缓存文件位置，以便在持续集成环境中将缓存持久化
+    .replace(
+      'module.exports =',
+      `
+  const appName = paths.appPath.replace(/\\//g, '.');
+  const webpackCacheDir = path.join(process.env['HOME'], '.webpack/cache', appName);
+  module.exports =`,
+    )
+    .replace(
+      /cacheDirectory:\s*true,/g,
+      `cacheDirectory: path.join(webpackCacheDir, 'babel-loader'),`,
+    )
+    .replace(
+      'eslintPath: ',
+      `cache: path.join(webpackCacheDir, 'eslint-loader'),
+                eslintPath: `,
     )
 
   fs.writeFileSync(configFile, content, { encoding: 'utf8' })
