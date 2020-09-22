d580a7bd059b96847817431e9580c2a88aa70890
bubkoo <bubkoo.wy@gmail.com>
2020-09-22 11:36:13 +0800

    test: ✅ dryrun

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,69 +1,3 @@
-// You can import your modules
-// import index from '../src/index'
-
-import nock from "nock";
-// Requiring our app implementation
-import myProbotApp from "../src";
-import { Probot, ProbotOctokit } from "probot";
-// Requiring our fixtures
-import payload from "./fixtures/issues.opened.json";
-const issueCreatedBody = { body: "Thanks for opening this issue!" };
-const fs = require("fs");
-const path = require("path");
-
-const privateKey = fs.readFileSync(
-  path.join(__dirname, "fixtures/mock-cert.pem"),
-  "utf-8"
-);
-
-describe("My Probot app", () => {
-  let probot: any;
-
-  beforeEach(() => {
-    nock.disableNetConnect();
-    probot = new Probot({
-      id: 123,
-      privateKey,
-      // disable request throttling and retries for testing
-      Octokit: ProbotOctokit.defaults({
-        retry: { enabled: false },
-        throttle: { enabled: false },
-      }),
-    });
-    // Load our app into probot
-    probot.load(myProbotApp);
-  });
-
-  test("creates a comment when an issue is opened", async (done) => {
-    const mock = nock("https://api.github.com")
-      // Test that we correctly return a test token
-      .post("/app/installations/2/access_tokens")
-      .reply(200, {
-        token: "test",
-        permissions: {
-          issues: "write",
-        },
-      })
-
-      // Test that a comment is posted
-      .post("/repos/hiimbex/testing-things/issues/1/comments", (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody));
-        return true;
-      })
-      .reply(200);
-
-    // Receive a webhook event
-    await probot.receive({ name: "issues", payload });
-
-    expect(mock.pendingMocks()).toStrictEqual([]);
-  });
-
-  afterEach(() => {
-    nock.cleanAll();
-    nock.enableNetConnect();
-  });
-});
-
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
 
@@ -72,3 +6,67 @@ describe("My Probot app", () => {
 
 // For more information about testing with Nock see:
 // https://github.com/nock/nock
+
+import fs from 'fs'
+import path from 'path'
+import nock from 'nock'
+import { Probot, ProbotOctokit } from 'probot'
+import app from '../src/app'
+
+// import payload from './fixtures/issues.opened.json'
+// const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+
+const privateKey = fs.readFileSync(
+  path.join(__dirname, 'fixtures/mock-cert.pem'),
+  'utf-8',
+)
+
+describe('bot', () => {
+  let probot: any
+
+  beforeEach(() => {
+    nock.disableNetConnect()
+    probot = new Probot({
+      privateKey,
+      id: 123,
+      // disable request throttling and retries for testing
+      Octokit: ProbotOctokit.defaults({
+        retry: { enabled: false },
+        throttle: { enabled: false },
+      }),
+    })
+    // Load our app into probot
+    probot.load(app)
+  })
+
+  it('ping', async (done) => {
+    const mock = nock('https://api.github.com')
+      // Test that we correctly return a test token
+      .post('/app/installations/2/access_tokens')
+      .reply(200, {
+        token: 'test',
+        permissions: {
+          issues: 'write',
+        },
+      })
+
+    // Test that a comment is posted
+    // .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
+    //   expect(body).toEqual(issueCreatedBody)
+    //   done()
+    //   return true
+    // })
+    // .reply(200)
+
+    // Receive a webhook event
+    // await probot.receive({ payload, name: 'issues' })
+
+    expect(mock.pendingMocks().length).toEqual(1)
+    done()
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+    nock.enableNetConnect()
+  })
+})
