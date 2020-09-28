f10adc37fbaea2451120f059967fb325aae8ec29
bubkoo <bubkoo.wy@gmail.com>
2020-09-28 08:58:55 +0800

    feat: ✨ dryrun

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import { Application } from 'probot'
-import { Welcome } from './welcome'
-import { AutoAssign } from './auto-assign'
-import { AutoComment } from './auto-comment'
-import { RequestInfo } from './request-info'
-import { LabelActions } from './label-actions'
+// import { Welcome } from './welcome'
+// import { AutoAssign } from './auto-assign'
+// import { AutoComment } from './auto-comment'
+// import { RequestInfo } from './request-info'
+// import { LabelActions } from './label-actions'
 
 export = (app: Application) => {
   app.on('*', async (context) => {
     context.log(`event: ${context.name}`)
   })
 
-  Welcome.start(app)
-  AutoComment.start(app)
-  AutoAssign.start(app)
-  RequestInfo.start(app)
-  LabelActions.start(app)
+  // Welcome.start(app)
+  // AutoComment.start(app)
+  // AutoAssign.start(app)
+  // RequestInfo.start(app)
+  // LabelActions.start(app)
 }
