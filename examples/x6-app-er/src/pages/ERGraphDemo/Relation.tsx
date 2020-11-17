ffbea86751f5c7ed008c4deaaffee908fc434622
小耀 <jinyue.gjy@antfin.com>
2020-11-17 11:10:47 +0800

    feat: ✨ ERGraphDemo 初步完成

---
+++ examples/x6-app-er/src/pages/ERGraphDemo/Relation.tsx
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import './Relation.less';
+
+class Relation extends PureComponent {
+  render() {
+    return <div className="relation-container">1 : N</div>;
+  }
+}
+
+export default Relation;
---
+++ examples/x6-app-er/src/pages/xflow/graph/edge.ts
@@ -0,0 +1,7 @@
+import { Edge as X6Edge } from '@antv/x6';
+
+export default class Edge extends X6Edge {
+  constructor() {
+    super();
+  }
+}
