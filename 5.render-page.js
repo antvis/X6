81efc3b06080860082f65bbb7d302b46e9642ad1
NewByVector <NewByVector@users.noreply.github.com>
2020-11-20 13:58:11 +0000

    deploy: b72ba7951e8208f002d90ec32b670a0c55c65d2c

--- 5.render-page.js
+++ 5.render-page.js
@@ -2,9 +2,9 @@ exports.ids = [5];
 exports.modules = {
 
 /***/ "../../node_modules/monaco-editor/esm/vs/basic-languages/typescript/typescript.js":
-/*!*******************************************************************************************************************!*\
-  !*** /Users/wenyu/vector/code/AntV/X6/node_modules/monaco-editor/esm/vs/basic-languages/typescript/typescript.js ***!
-  \*******************************************************************************************************************/
+/*!**********************************************************************************************************!*\
+  !*** /home/runner/work/X6/X6/node_modules/monaco-editor/esm/vs/basic-languages/typescript/typescript.js ***!
+  \**********************************************************************************************************/
 /*! exports provided: conf, language */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
