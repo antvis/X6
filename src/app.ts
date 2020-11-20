baa3ee11a745c6342717b930464eb50a7114dc93
bubkoo <bubkoo.wy@gmail.com>
2020-11-20 17:17:56 +0800

    feat: ✨ remove repo from an installation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
-import { Application } from 'probot'
+import { Application, Context } from 'probot'
 import { PRTriage } from './pr-triage'
 
 export = (app: Application) => {
-  app.on('*', async (context) => {
+  app.on('*', async (context: Context) => {
     context.log(`event: ${context.name}`)
+
+    const { data: repo } = await context.github.repos.get(context.repo())
+    const { data: app } = await context.github.apps.getBySlug({
+      app_slug: 'pr-triage',
+    })
+
+    await context.github.apps.removeRepoFromInstallation({
+      installation_id: app.id,
+      repository_id: repo.id,
+    })
+
+    context.log(repo)
+    context.log(app)
   })
 
   PRTriage.start(app)
