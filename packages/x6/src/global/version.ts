eed5e7fc590512be5293b67e92c69c33fae414fa
bubkoo <bubkoo.wy@gmail.com>
2020-11-25 15:32:36 +0800

    build: 📦️ update version

--- packages/x6/src/global/version.ts
+++ packages/x6/src/global/version.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 
 /**
-* Auto generated version file, do not modify it!
-*/
-const version = '1.0.6'
+ * Auto generated version file, do not modify it!
+ */
+const version = '1.0.7'
 export { version }
