97dc51f30a86b12a52d8b4d3cde7901616ccb490
bubkoo <bubkoo.wy@gmail.com>
2020-11-30 16:12:47 +0800

    fix: 🐛 inherit from poly

--- packages/x6/src/shape/standard/polygon.ts
+++ packages/x6/src/shape/standard/polygon.ts
@@ -1,9 +1,9 @@
+import { Base } from '../base'
 import { Poly } from './poly'
 import { createShape } from './util'
 
-export const Polygon = createShape('polygon', {
-  attrs: {
-    body: {},
-  },
-  parent: Poly,
-})
+export const Polygon = createShape(
+  'polygon',
+  {},
+  { parent: Poly as typeof Base },
+)
