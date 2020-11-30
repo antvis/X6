23d5f9431cfee3ab241569c9274f4ac79a2d12cc
bubkoo <bubkoo.wy@gmail.com>
2020-11-30 16:02:35 +0800

    fix: 🐛 remove default pathData

--- packages/x6/src/shape/standard/path.ts
+++ packages/x6/src/shape/standard/path.ts
@@ -1,13 +1,7 @@
 import { createShape } from './util'
 import { ObjectExt } from '../../util'
 
-export const Path = createShape('path', {
-  attrs: {
-    body: {
-      refD: 'M 0 0 L 10 0 10 10 0 10 Z',
-    },
-  },
-})
+export const Path = createShape('path', {})
 
 Path.config({
   propHooks(metadata) {
