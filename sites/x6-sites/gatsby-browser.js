b084b20a666da4d26af93e97994eec9be4df350d
bubkoo <bubkoo.wy@gmail.com>
2020-11-30 23:28:46 +0800

    chore(config): 🛠️ ⚙️ add deps

--- sites/x6-sites/gatsby-browser.js
+++ sites/x6-sites/gatsby-browser.js
@@ -2,4 +2,6 @@
  * Note the way of mounting dependencies
  * See: https://github.com/antvis/gatsby-theme-antv/issues/80
  */
+window.react = require('react')
 window.x6 = require('@antv/x6')
+window.x6ReactShape = require('@antv/x6-react-shape')
