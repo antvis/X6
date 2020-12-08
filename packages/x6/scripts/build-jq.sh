67a7e2cda4e9950c7f5b247769dcd43d97c12a71
bubkoo <bubkoo.wy@gmail.com>
2020-12-08 10:35:01 +0800

    build: 📦️ build custom jquery with custom modules

---
+++ packages/x6/scripts/build-jq.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+name=jquery
+version=3.5.1
+target=./dist
+
+cd node_modules
+# rm -rf $name
+# git clone https://github.com/jquery/jquery $name
+
+cd $name
+git checkout tags/$version -b $version
+yarn
+rm -rf $target
+grunt custom:-ajax,-callbacks,-data,-deprecated,-event/alias,-event/trigger,-queue,-core/ready,-deferred,-exports/global,-serialize,-wrap dist:$target
