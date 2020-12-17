4675975aa10395fc7c6827af4b08e6406eb4d93b
vector <vectorse@126.com>
2020-12-17 23:23:49 +0800

    feat: add x6-vue-shape

---
+++ packages/x6-vue-shape/rollup.config.js
@@ -0,0 +1,33 @@
+import { terser } from 'rollup-plugin-terser'
+import replace from '@rollup/plugin-replace'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import filesize from 'rollup-plugin-filesize'
+import typescript from '@rollup/plugin-typescript'
+import autoExternal from 'rollup-plugin-auto-external'
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      name: 'X6VueShape',
+      format: 'umd',
+      file: 'dist/x6-vue-shape.js',
+      sourcemap: true,
+      globals: {
+        vue: 'Vue',
+        'react-dom': 'ReactDom',
+        '@antv/x6': 'X6',
+      },
+    },
+  ],
+  plugins: [
+    typescript({ declaration: false }),
+    resolve(),
+    commonjs(),
+    replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    terser(),
+    filesize(),
+    autoExternal(),
+  ],
+}
