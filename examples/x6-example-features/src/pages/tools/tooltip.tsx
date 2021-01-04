3a1ab7c5a18ce8f86531bede9991e1068aeaf5e8
bubkoo <bubkoo.wy@gmail.com>
2021-01-04 11:56:35 +0800

    docs: 📚️ add contextmenu demo

--- examples/x6-example-features/src/pages/tools/tooltip.tsx
+++ examples/x6-example-features/src/pages/tools/tooltip.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { Tooltip } from 'antd'
 import { Graph, ToolsView, EdgeView } from '@antv/x6'
+import '../index.less'
 
 class TooltipTool extends ToolsView.ToolItem<EdgeView, TooltipToolOptions> {
   private delay = 100
@@ -85,57 +86,90 @@ export interface TooltipToolOptions extends ToolsView.ToolItem.Options {
 Graph.registerNodeTool('tooltip', TooltipTool, true)
 Graph.registerEdgeTool('tooltip', TooltipTool, true)
 
-const graph = new Graph({
-  container: document.getElementById('container'),
-  grid: true,
-})
+export default class Example extends React.Component {
+  private container: HTMLDivElement
 
-const edge1 = graph.addEdge({
-  source: { x: 40, y: 40 },
-  target: { x: 380, y: 40 },
-  vertices: [
-    { x: 40, y: 80 },
-    { x: 200, y: 80 },
-    { x: 200, y: 40 },
-  ],
-  attrs: {
-    line: {
-      stroke: '#3c4260',
-      strokeWidth: 2,
-      targetMarker: 'classic',
-    },
-  },
-})
+  componentDidMount() {
+    const graph = new Graph({
+      container: this.container,
+      width: 800,
+      height: 600,
+      grid: true,
+      panning: true,
+    })
 
-graph.addEdge({
-  source: { x: 40, y: 160 },
-  target: { x: 380, y: 160 },
-  vertices: [
-    { x: 40, y: 200 },
-    { x: 200, y: 200 },
-    { x: 200, y: 160 },
-  ],
-  attrs: {
-    line: {
-      stroke: '#3c4260',
-      strokeWidth: 2,
-      targetMarker: 'classic',
-    },
-  },
-  connector: 'smooth',
-})
-
-graph.on('cell:mouseenter', ({ cell }) => {
-  cell.addTools([
-    {
-      name: 'tooltip',
-      args: {
-        tooltip: cell === edge1 ? 'edge 1 tooltip' : 'edge 2 tooltip ',
+    const source = graph.addNode({
+      x: 180,
+      y: 60,
+      width: 100,
+      height: 40,
+      attrs: {
+        body: {
+          fill: '#f5f5f5',
+          stroke: '#d9d9d9',
+          strokeWidth: 1,
+        },
       },
-    },
-  ])
-})
+    })
 
-graph.on('cell:mouseleave', ({ cell }) => {
-  cell.removeTools()
-})
+    const target = graph.addNode({
+      x: 320,
+      y: 250,
+      width: 100,
+      height: 40,
+      attrs: {
+        body: {
+          fill: '#f5f5f5',
+          stroke: '#d9d9d9',
+          strokeWidth: 1,
+        },
+      },
+    })
+
+    graph.addEdge({
+      source,
+      target,
+      attrs: {
+        line: {
+          stroke: '#a0a0a0',
+          strokeWidth: 1,
+          targetMarker: {
+            name: 'classic',
+            size: 7,
+          },
+        },
+      },
+    })
+
+    graph.on('cell:mouseenter', ({ cell }) => {
+      cell.addTools([
+        {
+          name: 'tooltip',
+          args: {
+            tooltip: cell.isNode()
+              ? cell === source
+                ? 'source tooltip'
+                : 'target tooltip '
+              : 'edge tooltip',
+          },
+        },
+      ])
+    })
+
+    graph.on('cell:mouseleave', ({ cell }) => {
+      cell.removeTools()
+    })
+  }
+
+  refContainer = (container: HTMLDivElement) => {
+    this.container = container
+  }
+
+  render() {
+    return (
+      <div className="x6-graph-wrap">
+        <div ref={this.refContainer} className="x6-graph" />
+      </div>
+    )
+  }
+}
