0e6e7675e0c95216b727c6a121b4c96ee8b483ee
bubkoo <bubkoo.wy@gmail.com>
2021-01-06 10:51:36 +0800

    refactor: ♻️ add path bg

--- packages/x6/src/shape/standard/path.ts
+++ packages/x6/src/shape/standard/path.ts
@@ -1,9 +1,38 @@
-import { createShape } from './util'
+import { Node } from '../../model/node'
 import { ObjectExt } from '../../util'
+import { labelAttr } from './util'
 
-export const Path = createShape('path', {})
-
-Path.config({
+export const Path = Node.define({
+  shape: 'path',
+  markup: [
+    {
+      tagName: 'rect',
+      selector: 'bg',
+    },
+    {
+      tagName: 'path',
+      selector: 'body',
+    },
+    {
+      tagName: 'text',
+      selector: 'label',
+    },
+  ],
+  attrs: {
+    bg: {
+      refWidth: '100%',
+      refHeight: '100%',
+      fill: 'none',
+      stroke: 'none',
+      pointerEvents: 'all',
+    },
+    body: {
+      fill: 'none',
+      stroke: '#000',
+      strokeWidth: 2,
+    },
+    label: labelAttr,
+  },
   propHooks(metadata) {
     const { path, ...others } = metadata
     if (path) {
