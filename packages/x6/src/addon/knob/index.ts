13704297743133500b750a90a5c0f98e119380a4
bubkoo <bubkoo.wy@gmail.com>
2021-01-08 09:57:31 +0800

    feat: ✨ add `position` hook for knobs

--- packages/x6/src/addon/knob/index.ts
+++ packages/x6/src/addon/knob/index.ts
@@ -7,6 +7,7 @@ import { NodeView } from '../../view/node'
 import { EdgeView } from '../../view/edge'
 import { Graph } from '../../graph/graph'
 import { Dom } from '../../util'
+import { Angle, Point } from '../../geometry'
 
 export class Knob extends Widget<Knob.Options> {
   public container: HTMLDivElement
@@ -95,12 +96,33 @@ export class Knob extends Widget<Knob.Options> {
   }
 
   protected update() {
-    if (this.metadata && this.metadata.update) {
-      this.metadata.update.call(this.graph, {
+    if (this.metadata) {
+      const { update, position } = this.metadata
+      const args = {
         knob: this,
         cell: this.cell,
         node: this.node,
-      })
+      }
+
+      if (position) {
+        const pos = position.call(this.graph, { ...args })
+        if (pos) {
+          const ctm = this.graph.matrix()
+          const bbox = this.node.getBBox()
+          const angle = Angle.normalize(this.node.getAngle())
+          const local = Point.create(pos)
+          if (angle !== 0) {
+            local.rotate(-angle, { x: bbox.width / 2, y: bbox.height / 2 })
+          }
+          local.translate(bbox).scale(ctm.a, ctm.d).translate(ctm.e, ctm.f)
+          this.container.style.left = `${local.x}px`
+          this.container.style.top = `${local.y}px`
+        }
+      }
+
+      if (update) {
+        update.call(this.graph, { ...args })
+      }
     }
   }
 
@@ -173,6 +195,10 @@ export class Knob extends Widget<Knob.Options> {
 
     this.setEventData<EventData.Knob>(e, {
       knobbing: false,
+      originX: e.clientX,
+      originY: e.clientY,
+      clientX: e.clientX,
+      clientY: e.clientY,
     })
 
     this.graph.view.undelegateEvents()
@@ -202,10 +228,20 @@ export class Knob extends Widget<Knob.Options> {
       this.model.startBatch('knob', { cid: this.cid })
     }
 
+    data.clientX = e.clientX
+    data.clientY = e.clientY
+
     if (this.metadata && this.metadata.onMouseMove) {
+      const ctm = this.graph.matrix()
+      const dx = (e.clientX - data.originX) / ctm.a
+      const dy = (e.clientY - data.originY) / ctm.d
+      const angle = this.node.getAngle()
+      const delta = new Point(dx, dy).rotate(angle)
       this.metadata.onMouseMove.call(this.graph, {
         e,
         data,
+        deltaX: delta.x,
+        deltaY: delta.y,
         knob: this,
         cell: this.cell,
         node: this.node,
@@ -263,13 +299,17 @@ export namespace Knob {
   export interface Metadata {
     enabled?: boolean | ((this: Graph, node: Node) => boolean)
     update?: (this: Graph, args: UpdateArgs) => void
+    position?: (this: Graph, args: UpdateArgs) => Point.PointLike
     onMouseDown?: (
       this: Graph,
       args: HandlerArgs<JQuery.MouseDownEvent>,
     ) => void
     onMouseMove?: (
       this: Graph,
-      args: HandlerArgs<JQuery.MouseMoveEvent>,
+      args: HandlerArgs<JQuery.MouseMoveEvent> & {
+        deltaX: number
+        deltaY: number
+      },
     ) => void
     onMouseUp?: (this: Graph, args: HandlerArgs<JQuery.MouseUpEvent>) => void
   }
@@ -290,5 +330,9 @@ namespace Private {
 namespace EventData {
   export interface Knob extends KeyValue {
     knobbing: boolean
+    originX: number
+    originY: number
+    clientX: number
+    clientY: number
   }
 }
