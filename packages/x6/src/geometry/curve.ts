90a56037b16adff6fc3fbf50660eb95d3bd6bd2d
bubkoo <bubkoo.wy@gmail.com>
2021-01-08 10:02:47 +0800

    feat: ✨ add `rotate` method for geoemtrys

--- packages/x6/src/geometry/curve.ts
+++ packages/x6/src/geometry/curve.ts
@@ -705,6 +705,14 @@ export class Curve extends Geometry {
     return this
   }
 
+  rotate(angle: number, origin?: Point.PointLike | Point.PointData) {
+    this.start.rotate(angle, origin)
+    this.controlPoint1.rotate(angle, origin)
+    this.controlPoint2.rotate(angle, origin)
+    this.end.rotate(angle, origin)
+    return this
+  }
+
   translate(tx: number, ty: number): this
   translate(p: Point.PointLike | Point.PointData): this
   translate(tx: number | Point.PointLike | Point.PointData, ty?: number) {
