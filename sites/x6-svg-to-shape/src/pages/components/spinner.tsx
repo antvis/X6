d6c5816af4d273dd9ae194dc4f985aabb07dd113
bubkoo <bubkoo.wy@gmail.com>
2021-01-28 10:50:35 +0800

    feat: ✨ add app to convert svg to render code
    re #480

---
+++ sites/x6-svg-to-shape/src/pages/components/spinner.tsx
@@ -0,0 +1,12 @@
+import './spinner.less'
+
+export function Spinner() {
+  return (
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
