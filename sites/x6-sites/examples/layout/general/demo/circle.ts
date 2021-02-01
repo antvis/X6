5f4608500237f282fae36f927c76e9820d54bab5
vector <vectorse@126.com>
2021-02-01 09:50:28 +0800

    docs: add layout demo (#626)

---
+++ sites/x6-sites/examples/layout/general/demo/circle.ts
@@ -0,0 +1,34 @@
+import { Graph, Model } from '@antv/x6'
+import { Layout } from '@antv/layout'
+
+const data: Model.FromJSONData = {
+  nodes: [],
+}
+
+for (let i = 1; i <= 24; i++) {
+  data.nodes!.push({
+    id: `${i}`,
+    shape: 'rect',
+    width: 24,
+    height: 24,
+    label: '💜',
+    attrs: {
+      body: {
+        stroke: 'transparent',
+      },
+    },
+  })
+}
+
+const graph = new Graph({
+  container: document.getElementById('container')!,
+  grid: true,
+})
+
+const circularLayout = new Layout({
+  type: 'circular',
+  center: [350, 250],
+})
+const model = circularLayout.layout(data)
+
+graph.fromJSON(model)
