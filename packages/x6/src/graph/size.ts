949a42dacfc5023d25bcabc0a3a1a7d8578f1b96
bubkoo <bubkoo.wy@gmail.com>
2021-02-04 13:46:39 +0800

    fix: 🐛 do not update pagesize automatically when set graph size
    在 scroller 中,初始化画布时,pagesize 的默认大小为画布大小,初始化后如果手动将画布大小修改到比 pagesize 小,同时如果开启了 autoResize
    选项,移动节点等操作会自动判断是否需要修改画布大小,将导致手动修改画布大小的操作失效
    
    fix #644 re #564

--- packages/x6/src/graph/size.ts
+++ packages/x6/src/graph/size.ts
@@ -38,13 +38,7 @@ export class SizeManager extends Base {
   }
 
   resizeGraph(width?: number, height?: number) {
-    // update scroller's page size as needed
-    const scroller = this.graph.scroller.widget
-    if (scroller) {
-      scroller.updatePageSize(width, height)
-    }
     this.graph.transform.resize(width, height)
-    return this
   }
 
   resizeScroller(width?: number, height?: number) {
@@ -54,7 +48,7 @@ export class SizeManager extends Base {
   resizePage(width?: number, height?: number) {
     const instance = this.graph.scroller.widget
     if (instance) {
-      instance.updatePageSize(width, height, true)
+      instance.updatePageSize(width, height)
     }
   }
 
