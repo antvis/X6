e3d4ee9738ba0ee2b15a0563b80121f8ba6fdbe2
vector <vectorse@126.com>
2021-03-05 15:14:29 +0800

    Updates

---
+++ demos/tutorial/advanced/layout/circular/precache-manifest.907140ca1e8e9d8b4c05c78416f5896d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efd8a8e1d5543880b0eb8d2b0f20831",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e163d6eae0fa0fe483d9",
+    "url": "./static/css/2.66428cc9.chunk.css"
+  },
+  {
+    "revision": "155025501764498b6592",
+    "url": "./static/css/main.8a64f20d.chunk.css"
+  },
+  {
+    "revision": "e163d6eae0fa0fe483d9",
+    "url": "./static/js/2.4ee513a8.chunk.js"
+  },
+  {
+    "revision": "841beb52bcf1b82f8a3217aa26c934ca",
+    "url": "./static/js/2.4ee513a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155025501764498b6592",
+    "url": "./static/js/main.ec306758.chunk.js"
+  },
+  {
+    "revision": "7af54b05193274feb7e0",
+    "url": "./static/js/runtime-main.69a83e76.js"
+  }
+]);
---
+++ demos/tutorial/intermediate/interacting/connecting/precache-manifest.95debd9d8826048492428633171fce44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa1ffdc85833c73612ec553f6a76b05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e536ebb7ed5b507683e1",
+    "url": "./static/css/2.366a0d83.chunk.css"
+  },
+  {
+    "revision": "19e41490f457d54204a2",
+    "url": "./static/css/main.6622eeb9.chunk.css"
+  },
+  {
+    "revision": "e536ebb7ed5b507683e1",
+    "url": "./static/js/2.48b2a385.chunk.js"
+  },
+  {
+    "revision": "1c2c8f5d7420e01765de4e8e61239efe",
+    "url": "./static/js/2.48b2a385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e41490f457d54204a2",
+    "url": "./static/js/main.12b999c1.chunk.js"
+  },
+  {
+    "revision": "4e92f08ff2f9dcd6098c",
+    "url": "./static/js/runtime-main.25db9659.js"
+  }
+]);
