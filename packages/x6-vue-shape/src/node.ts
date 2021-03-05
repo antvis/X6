216e3fef00f7694400584a790a895dc4ecd00b2e
vector <vectorse@126.com>
2021-03-05 12:21:23 +0800

    fix: 🐛 add primer for vue-shape (#738)

--- packages/x6-vue-shape/src/node.ts
+++ packages/x6-vue-shape/src/node.ts
@@ -1,4 +1,4 @@
-import { Node, Markup } from '@antv/x6'
+import { Node, Markup, ObjectExt } from '@antv/x6'
 import { Definition } from './registry'
 
 export class VueShape<
@@ -35,27 +35,49 @@ export class VueShape<
 }
 
 export namespace VueShape {
+  export type Primer =
+    | 'rect'
+    | 'circle'
+    | 'path'
+    | 'ellipse'
+    | 'polygon'
+    | 'polyline'
+
   export interface Properties extends Node.Properties {
+    primer?: Primer
+    useForeignObject?: boolean
     component?: Definition | string
   }
 }
 
 export namespace VueShape {
-  VueShape.config({
-    view: 'vue-shape-view',
-    markup: [
+  function getMarkup(useForeignObject: boolean, primer: Primer = 'rect') {
+    const markup: Markup.JSONMarkup[] = [
       {
-        tagName: 'rect',
+        tagName: primer,
         selector: 'body',
       },
-      {
-        ...Markup.getForeignObjectMarkup(),
-      },
-      {
-        tagName: 'text',
-        selector: 'label',
-      },
-    ],
+    ]
+
+    if (useForeignObject) {
+      markup.push(Markup.getForeignObjectMarkup())
+    } else {
+      markup.push({
+        tagName: 'g',
+        selector: 'content',
+      })
+    }
+
+    markup.push({
+      tagName: 'text',
+      selector: 'label',
+    })
+
+    return markup
+  }
+  VueShape.config({
+    view: 'vue-shape-view',
+    markup: getMarkup(true),
     attrs: {
       body: {
         fill: 'none',
@@ -75,6 +97,46 @@ export namespace VueShape {
         textAnchor: 'middle',
         textVerticalAnchor: 'middle',
       },
+    },
+    propHooks(metadata: Properties) {
+      if (metadata.markup == null) {
+        const primer = metadata.primer
+        const useForeignObject = metadata.useForeignObject
+        if (primer != null || useForeignObject != null) {
+          metadata.markup = getMarkup(useForeignObject !== false, primer)
+          if (primer) {
+            if (metadata.attrs == null) {
+              metadata.attrs = {}
+            }
+            let attrs = {}
+            if (primer === 'circle') {
+              attrs = {
+                refCx: '50%',
+                refCy: '50%',
+                refR: '50%',
+              }
+            } else if (primer === 'ellipse') {
+              attrs = {
+                refCx: '50%',
+                refCy: '50%',
+                refRx: '50%',
+                refRy: '50%',
+              }
+            }
+
+            if (primer !== 'rect') {
+              metadata.attrs = ObjectExt.merge({}, metadata.attrs, {
+                body: {
+                  refWidth: null,
+                  refHeight: null,
+                  ...attrs,
+                },
+              })
+            }
+          }
+        }
+      }
+      return metadata
     },
   })
 
