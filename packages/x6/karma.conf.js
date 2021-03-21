7a56a70c1efdad6157e3023103f43b78341b5b42
bubkoo <bubkoo.wy@gmail.com>
2021-03-22 01:39:48 +0800

    chore: 🛠️ sharing karma configurations

--- packages/x6/karma.conf.js
+++ packages/x6/karma.conf.js
@@ -1,70 +1,11 @@
-const es6transform = require('karma-typescript-es6-transform')
-
-module.exports = (config) => {
-  config.set({
-    frameworks: ['jasmine', 'karma-typescript'],
-    files: [{ pattern: 'src/**/*.ts' }],
-    preprocessors: {
-      '**/*.ts': ['karma-typescript'],
+module.exports = (config) =>
+  require('../../configs/karma-config.js')(
+    config,
+    {
+      files: [{ pattern: 'src/**/*.ts' }],
+      logLevel: config.LOG_DEBUG,
     },
-    reporters: ['spec', 'karma-typescript'],
-    browsers: [process.env.CI ? 'ChromeHeadless' : 'Chrome'],
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--no-sandbox',
-          '--disable-gpu',
-          '--disable-translate',
-          '--disable-extensions',
-          '--remote-debugging-port=9222',
-        ],
-      },
-    },
-    karmaTypescriptConfig: {
-      tsconfig: './tsconfig.json',
+    {
       include: ['./src/**/*.ts', '../../node_modules/csstype/**/*'],
-      bundlerOptions: {
-        sourceMap: true,
-        transforms: [
-          es6transform({
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    node: 'current',
-                  },
-                },
-              ],
-            ],
-          }),
-        ],
-      },
-      coverageOptions: {
-        instrumentation: true,
-        exclude: /\.test\.ts$/,
-      },
-      reports: {
-        html: 'test/coverage',
-        lcovonly: {
-          directory: 'test/coverage',
-          filename: 'lcov.info',
-          subdirectory: './',
-        },
-        'text-summary': '',
-      },
     },
-    client: {
-      jasmine: {
-        random: false,
-      },
-    },
-    port: 9876,
-    colors: true,
-    autoWatch: true,
-    singleRun: true,
-    concurrency: Infinity,
-  })
-}
+  )
