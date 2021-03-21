b7c4076b931d8cf6bc01a878b095c32cb3fb2b3c
bubkoo <bubkoo.wy@gmail.com>
2021-03-21 21:12:41 +0800

    build: 📦️ sharing rollup configurations

--- packages/x6-vue-shape/rollup.config.js
+++ packages/x6-vue-shape/rollup.config.js
@@ -1,13 +1,7 @@
-import { terser } from 'rollup-plugin-terser'
-import replace from '@rollup/plugin-replace'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import filesize from 'rollup-plugin-filesize'
-import typescript from '@rollup/plugin-typescript'
 import autoExternal from 'rollup-plugin-auto-external'
+import config from '../../configs/rollup-config'
 
-export default {
-  input: './src/index.ts',
+export default config({
   output: [
     {
       name: 'X6VueShape',
@@ -22,16 +16,5 @@ export default {
       },
     },
   ],
-  plugins: [
-    typescript({ declaration: false }),
-    resolve(),
-    commonjs(),
-    replace({
-      preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    terser(),
-    filesize(),
-    autoExternal(),
-  ],
-}
+  plugins: [autoExternal()],
+})
