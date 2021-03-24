c26c8d20d19af4e55a808ba749b2a00b777cd4d0
luzhuang <364439895@qq.com>
2021-03-24 16:58:36 +0800

    fix: 修复x6-react-component antd引入的问题

--- packages/x6-react-components/src/color-picker/index.tsx
+++ packages/x6-react-components/src/color-picker/index.tsx
@@ -4,7 +4,7 @@
 import React from 'react'
 import classNames from 'classnames'
 import { Popover } from 'antd'
-import { PopoverProps } from 'antd/lib/popover'
+import { PopoverProps } from 'antd/es/popover'
 import 'antd/es/popover/style'
 import addEventListener from 'rc-util/lib/Dom/addEventListener'
 import {
