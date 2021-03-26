2e6a1f92b400b57866de463ef3b69bd0cc32a700
breezefaith <nyzhangzc@qq.com>
2021-03-26 11:34:45 +0800

    fix: 🐛 x6-app-draw支持windows下ctrl组合快捷键

--- examples/x6-app-draw/src/pages/components/ToolBar/index.tsx
+++ examples/x6-app-draw/src/pages/components/ToolBar/index.tsx
@@ -59,35 +59,35 @@ export default function () {
       setCanRedo(history.canRedo())
     })
 
-    graph.bindKey('meta+z', () => {
+    graph.bindKey(['meta+z', 'ctrl+z'], () => {
       if (history.canUndo()) {
         history.undo()
       }
       return false
     })
-    graph.bindKey('meta+shift+z', () => {
+    graph.bindKey(['meta+shift+z', 'ctrl+y'], () => {
       if (history.canRedo()) {
         history.redo()
       }
       return false
     })
-    graph.bindKey('meta+d', () => {
+    graph.bindKey(['meta+d', 'ctrl+d'], () => {
       graph.clearCells()
       return false
     })
-    graph.bindKey('meta+s', () => {
+    graph.bindKey(['meta+s', 'ctrl+s'], () => {
       graph.toPNG((datauri: string) => {
         DataUri.downloadDataUri(datauri, 'chart.png')
       })
       return false
     })
-    graph.bindKey('meta+p', () => {
+    graph.bindKey(['meta+p', 'ctrl+p'], () => {
       graph.printPreview()
       return false
     })
-    graph.bindKey('meta+c', copy)
-    graph.bindKey('meta+v', paste)
-    graph.bindKey('meta+x', cut)
+    graph.bindKey(['meta+c', 'ctrl+c'], copy)
+    graph.bindKey(['meta+v', 'ctrl+v'], paste)
+    graph.bindKey(['meta+x', 'ctrl+x'], cut)
   }, [])
 
   const handleClick = (name: string) => {
@@ -131,38 +131,38 @@ export default function () {
           <Item
             name="delete"
             icon={<ClearOutlined />}
-            tooltip="Clear (Cmd + D)"
+            tooltip="Clear (Cmd + D, Ctrl + D)"
           />
         </Group>
         <Group>
           <Item
             name="undo"
-            tooltip="Undo (Cmd + Z)"
+            tooltip="Undo (Cmd + Z, Ctrl + Z)"
             icon={<UndoOutlined />}
             disabled={!canUndo}
           />
           <Item
             name="redo"
-            tooltip="Redo (Cmd + Shift + Z)"
+            tooltip="Redo (Cmd + Shift + Z, Ctrl + Y)"
             icon={<RedoOutlined />}
             disabled={!canRedo}
           />
         </Group>
         <Group>
-          <Item name="copy" tooltip="Copy (Cmd + C)" icon={<CopyOutlined />} />
-          <Item name="cut" tooltip="Cut (Cmd + X)" icon={<ScissorOutlined />} />
+          <Item name="copy" tooltip="Copy (Cmd + C, Ctrl + C)" icon={<CopyOutlined />} />
+          <Item name="cut" tooltip="Cut (Cmd + X, Ctrl + X)" icon={<ScissorOutlined />} />
           <Item
             name="paste"
-            tooltip="Paste (Cmd + V)"
+            tooltip="Paste (Cmd + V, Ctrl + V)"
             icon={<SnippetsOutlined />}
           />
         </Group>
         <Group>
-          <Item name="save" icon={<SaveOutlined />} tooltip="Save (Cmd + S)" />
+          <Item name="save" icon={<SaveOutlined />} tooltip="Save (Cmd + S, Ctrl + S)" />
           <Item
             name="print"
             icon={<PrinterOutlined />}
-            tooltip="Print (Cmd + P)"
+            tooltip="Print (Cmd + P, Ctrl + P)"
           />
         </Group>
       </Toolbar>
