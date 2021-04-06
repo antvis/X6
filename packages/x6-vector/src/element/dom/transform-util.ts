2a516a87733513a2833871e25c99bddae05cf768
bubkoo <bubkoo.wy@gmail.com>
2021-04-06 09:39:15 +0800

    refactor: ♻️ move transform to dom

---
+++ packages/x6-vector/src/element/dom/transform-util.ts
@@ -0,0 +1,53 @@
+import { Global } from '../../global'
+import type { Matrix } from '../../struct/matrix'
+import type { VectorElement } from '../element'
+import type { Transform } from './transform'
+
+export namespace Util {
+  export function getTransformOrigin(
+    o: Matrix.TransformOptions,
+    t: Transform<Node>,
+  ): [number, number] {
+    // First check if origin is in ox or originX
+    let ox = o.ox != null ? o.ox : o.originX != null ? o.originX : 'center'
+    let oy = o.oy != null ? o.oy : o.originY != null ? o.originY : 'center'
+
+    // Then check if origin was used and overwrite in that case
+    const { origin } = o
+    if (origin != null) {
+      ;[ox, oy] = Array.isArray(origin)
+        ? origin
+        : typeof origin === 'object'
+        ? [origin.x, origin.y]
+        : [origin, origin]
+    }
+
+    // Make sure to only call bbox when actually needed
+    if (typeof ox === 'string' || typeof oy === 'string') {
+      const node = t.node
+      const { height, width, x, y } =
+        node instanceof Global.window.SVGElement
+          ? ((t as any) as VectorElement).bbox()
+          : (node as HTMLElement).getBoundingClientRect()
+
+      // And only overwrite if string was passed for this specific axis
+      if (typeof ox === 'string') {
+        ox = ox.includes('left')
+          ? x
+          : ox.includes('right')
+          ? x + width
+          : x + width / 2
+      }
+
+      if (typeof oy === 'string') {
+        oy = oy.includes('top')
+          ? y
+          : oy.includes('bottom')
+          ? y + height
+          : y + height / 2
+      }
+    }
+
+    return [ox, oy]
+  }
+}
