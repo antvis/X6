eb6adb4d7f1f6705b540b8f88e8251086c3473aa
bubkoo <bubkoo.wy@gmail.com>
2021-04-11 19:51:15 +0800

    fix: 🐛 do not overwrite constructor when apply mixins

--- packages/x6-vector/src/util/mixin.ts
+++ packages/x6-vector/src/util/mixin.ts
@@ -3,11 +3,13 @@ import { Class } from '../types'
 export function applyMixins(target: Class, ...sources: Class[]) {
   sources.forEach((source) => {
     Object.getOwnPropertyNames(source.prototype).forEach((name) => {
-      Object.defineProperty(
-        target.prototype,
-        name,
-        Object.getOwnPropertyDescriptor(source.prototype, name)!,
-      )
+      if (name !== 'constructor') {
+        Object.defineProperty(
+          target.prototype,
+          name,
+          Object.getOwnPropertyDescriptor(source.prototype, name)!,
+        )
+      }
     })
   })
 }
