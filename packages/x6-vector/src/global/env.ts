6326d7677c8036cde460361f7c95e6108c80e008
bubkoo <bubkoo.wy@gmail.com>
2021-04-11 21:56:37 +0800

    refactor: ♻️ remove unused env flags

--- packages/x6-vector/src/global/env.ts
+++ packages/x6-vector/src/global/env.ts
@@ -1,9 +1,3 @@
-import { Global } from './global'
-
 export namespace Env {
   export const isDev = process.env.NODE_ENV === 'development'
-
-  export function isIE() {
-    return (Global.document as any).documentMode != null
-  }
 }
