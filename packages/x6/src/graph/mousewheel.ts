247e977fc94888fd63c8fbfcf383cdba92272ef4
vector <vectorse@126.com>
2021-05-06 10:13:25 +0800

    fix: 🐛 fix can not scroller by mousewheel

--- packages/x6/src/graph/mousewheel.ts
+++ packages/x6/src/graph/mousewheel.ts
@@ -1,17 +1,21 @@
+import JQuery from 'jquery'
 import { Graph } from './graph'
 import { ModifierKey } from '../types'
-import { Dom, NumberExt } from '../util'
+import { Dom, NumberExt, Platform } from '../util'
 import { Disposable, IDisablable } from '../common'
 
 export class MouseWheel extends Disposable implements IDisablable {
   public readonly target: HTMLElement | Document
   public readonly container: HTMLElement
 
+  protected frameId: number | null
   protected cumulatedFactor = 1
   protected currentScale: number | null
   protected startPos: { x: number; y: number }
-
-  private mousewheelHandle: Dom.MouseWheelHandle
+  protected eventName: 'mousewheel' | 'wheel'
+  protected readonly handler: (
+    e: JQueryMousewheel.JQueryMousewheelEventObject,
+  ) => any
 
   protected get graph() {
     return this.options.graph
@@ -23,10 +27,8 @@ export class MouseWheel extends Disposable implements IDisablable {
     const scroller = this.graph.scroller.widget
     this.container = scroller ? scroller.container : this.graph.container
     this.target = this.options.global ? document : this.container
-    this.mousewheelHandle = new Dom.MouseWheelHandle(
-      this.target,
-      this.onMouseWheel.bind(this),
-    )
+    this.eventName = Platform.isEventSupported('wheel') ? 'wheel' : 'mousewheel'
+    this.handler = this.onMouseWheel.bind(this)
     if (this.options.enabled) {
       this.enable(true)
     }
@@ -40,7 +42,13 @@ export class MouseWheel extends Disposable implements IDisablable {
     if (this.disabled || force) {
       this.options.enabled = true
       this.graph.options.mousewheel.enabled = true
-      this.mousewheelHandle.enable()
+      if (Platform.SUPPORT_PASSIVE) {
+        this.target.addEventListener(this.eventName, this.handler, {
+          passive: false,
+        })
+      } else {
+        JQuery(this.target).on('mousewheel', this.handler)
+      }
     }
   }
 
@@ -48,7 +56,11 @@ export class MouseWheel extends Disposable implements IDisablable {
     if (!this.disabled) {
       this.options.enabled = false
       this.graph.options.mousewheel.enabled = false
-      this.mousewheelHandle.disable()
+      if (Platform.SUPPORT_PASSIVE) {
+        this.target.removeEventListener(this.eventName, this.handler)
+      } else {
+        JQuery(this.target).off('mousewheel')
+      }
     }
   }
 
@@ -60,6 +72,14 @@ export class MouseWheel extends Disposable implements IDisablable {
       (guard == null || guard.call(this.graph, e)) &&
       ModifierKey.isMatch(e, this.options.modifiers)
     ) {
+      evt.preventDefault()
+      evt.stopPropagation()
+
+      if (this.frameId) {
+        Dom.cancelAnimationFrame(this.frameId)
+        this.frameId = null
+      }
+
       const factor = this.options.factor || 1.2
 
       if (this.currentScale == null) {
@@ -104,38 +124,42 @@ export class MouseWheel extends Disposable implements IDisablable {
           this.currentScale,
       )
 
-      const scroller = this.graph.scroller.widget
-      const currentScale = this.currentScale!
-      let targetScale = this.graph.transform.clampScale(
-        currentScale * this.cumulatedFactor,
-      )
-      const minScale = this.options.minScale || Number.MIN_SAFE_INTEGER
-      const maxScale = this.options.maxScale || Number.MAX_SAFE_INTEGER
-      targetScale = NumberExt.clamp(targetScale, minScale, maxScale)
+      this.frameId = Dom.requestAnimationFrame(() => {
+        const scroller = this.graph.scroller.widget
+        const currentScale = this.currentScale!
+        let targetScale = this.graph.transform.clampScale(
+          currentScale * this.cumulatedFactor,
+        )
+        const minScale = this.options.minScale || Number.MIN_SAFE_INTEGER
+        const maxScale = this.options.maxScale || Number.MAX_SAFE_INTEGER
+        targetScale = NumberExt.clamp(targetScale, minScale, maxScale)
 
-      if (targetScale !== currentScale) {
-        if (scroller) {
-          if (this.options.zoomAtMousePosition) {
-            const origin = this.graph.coord.clientToLocalPoint(this.startPos)
-            scroller.zoom(targetScale, {
+        if (targetScale !== currentScale) {
+          if (scroller) {
+            if (this.options.zoomAtMousePosition) {
+              const origin = this.graph.coord.clientToLocalPoint(this.startPos)
+              scroller.zoom(targetScale, {
+                absolute: true,
+                center: origin.clone(),
+              })
+            } else {
+              scroller.zoom(targetScale, { absolute: true })
+            }
+          } else if (this.options.zoomAtMousePosition) {
+            const origin = this.graph.coord.clientToGraphPoint(this.startPos)
+            this.graph.zoom(targetScale, {
               absolute: true,
               center: origin.clone(),
             })
           } else {
-            scroller.zoom(targetScale, { absolute: true })
+            this.graph.zoom(targetScale, { absolute: true })
           }
-        } else if (this.options.zoomAtMousePosition) {
-          const origin = this.graph.coord.clientToGraphPoint(this.startPos)
-          this.graph.zoom(targetScale, {
-            absolute: true,
-            center: origin.clone(),
-          })
-        } else {
-          this.graph.zoom(targetScale, { absolute: true })
         }
-      }
-      this.currentScale = null
-      this.cumulatedFactor = 1
+
+        this.frameId = null
+        this.currentScale = null
+        this.cumulatedFactor = 1
+      })
     }
   }
 
