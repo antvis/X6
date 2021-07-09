5c6ac2fa1ab4c123023af63eec31397b972af33f
bubkoo <bubkoo.wy@gmail.com>
2021-07-09 16:16:11 +0800

    build: 📦️ build angular shape

--- packages/x6-angular-shape/rollup.config.js
+++ packages/x6-angular-shape/rollup.config.js
@@ -9,9 +9,12 @@ export default config({
       sourcemap: true,
       globals: {
         angular: 'Angular',
+        '@angular/cdk': 'AngularCDK',
+        '@angular/core': 'AngularCore',
+        '@angular/common': 'AngularCommon',
         '@antv/x6': 'X6',
       },
     },
   ],
-  external: ['@antv/x6'],
+  external: ['@antv/x6', '@angular/cdk', '@angular/core', '@angular/common'],
 })
