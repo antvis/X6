0f2a059be8da09e7bf46d61355db6813e7ba279d
bubkoo <bubkoo.wy@gmail.com>
2021-07-09 16:16:58 +0800

    refactor: ♻️ auto format by eslint tool

--- packages/x6-angular-shape/src/hook.ts
+++ packages/x6-angular-shape/src/hook.ts
@@ -1,30 +1,31 @@
-import { FunctionExt, Graph } from '@antv/x6';
-import { ContentArgs, registry } from './registry';
-import { AngularShape } from './node';
+import { FunctionExt, Graph } from '@antv/x6'
+import { ContentArgs, registry } from './registry'
+import { AngularShape } from './node'
 
 declare module '@antv/x6/lib/graph/hook' {
   namespace Hook {
     interface IHook {
-      getAngularContent(this: Graph, node: AngularShape): ContentArgs;
+      getAngularContent(this: Graph, node: AngularShape): ContentArgs
     }
   }
 
   interface Hook {
-    getAngularContent(node: AngularShape): ContentArgs;
+    getAngularContent(node: AngularShape): ContentArgs
   }
 }
 
 Graph.Hook.prototype.getAngularContent = function (node: AngularShape) {
-  let name = node.getComponentName();
+  const name = node.getComponentName()
   if (!name) {
-    throw new Error(`x6-angular-shape: You have to pass param 'componentName'!`);
+    throw new Error(`x6-angular-shape: You have to pass param 'componentName'!`)
   }
-  const content = registry.get(name);
+  const content = registry.get(name)
   if (content == null) {
-    return registry.onNotFound(name);
-  } else if (typeof content === 'function') {
-    const contentArgs = FunctionExt.call(content, this.graph, node as any);
-    return contentArgs;
+    return registry.onNotFound(name)
   }
-  return content;
-};
+  if (typeof content === 'function') {
+    const contentArgs = FunctionExt.call(content, this.graph, node as any)
+    return contentArgs
+  }
+  return content
+}
--- packages/x6-angular-shape/src/view.ts
+++ packages/x6-angular-shape/src/view.ts
@@ -1,70 +1,84 @@
-import { ApplicationRef, ComponentFactoryResolver, TemplateRef, ViewContainerRef } from '@angular/core';
-import { NodeView, Scheduler } from '@antv/x6';
-import { AngularShape } from './node';
-import { ComponentPortal, DomPortalOutlet, TemplatePortal } from '@angular/cdk/portal';
+import {
+  ApplicationRef,
+  ComponentFactoryResolver,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core'
+import { NodeView, Scheduler } from '@antv/x6'
+import {
+  ComponentPortal,
+  DomPortalOutlet,
+  TemplatePortal,
+} from '@angular/cdk/portal'
+import { AngularShape } from './node'
 
 export class AngularShapeView extends NodeView<AngularShape> {
   protected init() {
-    super.init();
+    super.init()
   }
 
   getContentContainer() {
-    return this.selectors.foContent as HTMLDivElement;
+    return this.selectors.foContent as HTMLDivElement
   }
 
   confirmUpdate(flag: number) {
-    const ret = super.confirmUpdate(flag);
+    const ret = super.confirmUpdate(flag)
     return this.handleAction(ret, AngularShapeView.action, () => {
-      Scheduler.scheduleTask(() => this.renderAngularContent());
-    });
+      Scheduler.scheduleTask(() => this.renderAngularContent())
+    })
   }
 
   protected renderAngularContent() {
-    this.unmountAngularContent();
-    const root = this.getContentContainer();
+    this.unmountAngularContent()
+    const root = this.getContentContainer()
     if (root) {
-      const node = this.cell;
-      const { injector, content } = this.graph.hook.getAngularContent(node);
-      const applicationRef = injector.get(ApplicationRef);
-      const viewContainerRef = injector.get(ViewContainerRef);
-      const componentFactoryResolver = injector.get(ComponentFactoryResolver);
-      const domOutlet = new DomPortalOutlet(root, componentFactoryResolver, applicationRef, injector);
+      const node = this.cell
+      const { injector, content } = this.graph.hook.getAngularContent(node)
+      const applicationRef = injector.get(ApplicationRef)
+      const viewContainerRef = injector.get(ViewContainerRef)
+      const componentFactoryResolver = injector.get(ComponentFactoryResolver)
+      const domOutlet = new DomPortalOutlet(
+        root,
+        componentFactoryResolver,
+        applicationRef,
+        injector,
+      )
       if (content instanceof TemplateRef) {
-        const portal = new TemplatePortal(content, viewContainerRef);
-        domOutlet.attachTemplatePortal(portal);
+        const portal = new TemplatePortal(content, viewContainerRef)
+        domOutlet.attachTemplatePortal(portal)
       } else {
-        const portal = new ComponentPortal(content as any, viewContainerRef);
-        domOutlet.attachComponentPortal(portal);
+        const portal = new ComponentPortal(content as any, viewContainerRef)
+        domOutlet.attachComponentPortal(portal)
       }
     }
   }
 
   protected unmountAngularContent() {
-    const root = this.getContentContainer();
-    root.innerHTML = '';
-    return root;
+    const root = this.getContentContainer()
+    root.innerHTML = ''
+    return root
   }
 
   unmount() {
-    this.unmountAngularContent();
-    return this;
+    this.unmountAngularContent()
+    return this
   }
 
   @NodeView.dispose()
   dispose() {
-    this.unmountAngularContent();
+    this.unmountAngularContent()
   }
 }
 
 export namespace AngularShapeView {
-  export const action = 'angular' as any;
+  export const action = 'angular' as any
 
   AngularShapeView.config({
     bootstrap: [action],
     actions: {
-      component: action
-    }
-  });
+      component: action,
+    },
+  })
 
-  NodeView.registry.register('angular-shape-view', AngularShapeView, true);
+  NodeView.registry.register('angular-shape-view', AngularShapeView, true)
 }
