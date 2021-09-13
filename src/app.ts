cf75d11721f0fa9b507af35ae3999816ee9f85d6
bubkoo <bubkoo.wy@gmail.com>
2021-09-13 16:14:15 +0800

    feat: ✨ disable WIP and PRTriage

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import { Application, Context } from 'probot'
-import { WIP } from './wip'
-import { PRTriage } from './pr-triage'
+// import { WIP } from './wip'
+// import { PRTriage } from './pr-triage'
 
 export = (app: Application) => {
   app.on('*', async (context: Context) => {
     context.log(`event: ${context.name}`)
   })
 
-  WIP.start(app)
-  PRTriage.start(app)
+  // WIP.start(app)
+  // PRTriage.start(app)
 }
