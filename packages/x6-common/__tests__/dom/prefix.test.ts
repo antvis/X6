07c5324767146ca738a5312440d1465623a7031b
newbyvector <vectorse@126.com>
2022-03-31 23:45:58 +0800

    feat: ✨ add css method for dom

--- packages/x6-common/__tests__/dom/prefix.test.ts
+++ packages/x6-common/__tests__/dom/prefix.test.ts
@@ -3,7 +3,7 @@ import { Dom } from '../../src/dom'
 describe('Dom', () => {
   describe('#prefix', () => {
     it('should return prefixed name with compatibility name', () => {
-      expect(Dom.getVendorPrefixedName('userDrag')).toBe('WebkitUserDrag')
+      expect(Dom.getVendorPrefixedName('userDrag')).toBe('webkitUserDrag')
     })
   })
 })
