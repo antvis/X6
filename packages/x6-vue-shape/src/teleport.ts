063777b7f26abfefffb9e3e6a3d20591e33b3bbe
lloydzhou <lloydzhou@qq.com>
2022-05-05 17:18:34 +0800

    feat: ✨ 兼容vue2.x

--- packages/x6-vue-shape/src/teleport.ts
+++ packages/x6-vue-shape/src/teleport.ts
@@ -1,84 +1,97 @@
-import { defineComponent, reactive, h, Teleport, markRaw } from 'vue'
+import { defineComponent, h, reactive, isVue3, Vue } from 'vue-demi'
 import { Graph, NodeView } from '@antv/x6'
 import { VueShape } from './node'
-
-const action: any = 'vue'
+import { VueShapeView } from './view'
 
 export function useTeleport(uniqViewId: string) {
-  const items = reactive<{ [key: string]: any }>({})
+  if (isVue3) {
+    const { Teleport, markRaw, Fragment, VNode, VNodeData } = Vue as any
+    const action: any = 'vue'
 
-  const connect = (
-    id: string,
-    node: VueShape,
-    graph: Graph,
-    component: any,
-    getContainer: () => HTMLDivElement,
-  ) => {
-    items[id] = markRaw(
-      defineComponent({
-        render: () =>
-          getContainer()
-            ? h(Teleport, { to: getContainer() }, h(component as any))
-            : null,
-        provide: () => ({
-          getGraph: () => graph,
-          getNode: () => node,
+    const items = reactive<{ [key: string]: any }>({})
+
+    const TeleportContainer = defineComponent({
+      setup() {
+        return () =>
+          h(
+            Fragment,
+            {},
+            Object.keys(items).map((id) => h(items[id])),
+          )
+      },
+    })
+
+    const connect = (
+      id: string,
+      node: VueShape,
+      graph: Graph,
+      component: any,
+      getContainer: () => HTMLDivElement,
+    ) => {
+      items[id] = markRaw(
+        defineComponent({
+          render: () =>
+            (getContainer()
+              ? h(Teleport, { to: getContainer() } as typeof VNodeData, [
+                  h(component),
+                ])
+              : null) as typeof VNode,
+          provide: () => ({
+            getGraph: () => graph,
+            getNode: () => node,
+          }),
         }),
-      }),
-    )
-  }
-  const disconnect = (id: string) => {
-    delete items[id]
-  }
-
-  const TeleportContainer = defineComponent({
-    setup() {
-      return () =>
-        h(
-          'div',
-          { style: 'display: none;' },
-          Object.keys(items).map((id) => h(items[id])),
-        )
-    },
-  })
-
-  class VuePortalShapeView extends NodeView<VueShape> {
-    init() {
-      super.init()
-      const targetId = `${this.graph.view.cid}:${this.cell.id}`
-      this.cell.on('removed', () => {
-        disconnect(targetId)
-      })
-      const component = this.graph.hook.getVueComponent(this.cell)
-      // 这里需要将当前View的cell以及graph还有component等对象存储起来给TeleportContainer使用
-      connect(
-        targetId,
-        this.cell,
-        this.graph,
-        component,
-        this.getComponentContainer.bind(this),
       )
     }
-    getComponentContainer() {
-      return this.cell.prop('useForeignObject') === false
-        ? (this.selectors.content as SVGElement)
-        : (this.selectors.foContent as HTMLDivElement)
+    const disconnect = (id: string) => {
+      delete items[id]
     }
-    confirmUpdate(flag: any) {
-      const ret = super.confirmUpdate(flag)
-      return this.handleAction(ret, action, () => {
-        // 这里无需做任何处理，但是，没有这个函数的时候，会卡死...
-      })
+
+    class VuePortalShapeView extends NodeView<VueShape> {
+      init() {
+        super.init()
+        const targetId = `${this.graph.view.cid}:${this.cell.id}`
+        this.cell.on('removed', () => {
+          disconnect(targetId)
+        })
+        const component = this.graph.hook.getVueComponent(this.cell)
+        // 这里需要将当前View的cell以及graph还有component等对象存储起来给TeleportContainer使用
+        connect(
+          targetId,
+          this.cell,
+          this.graph,
+          component,
+          this.getComponentContainer.bind(this),
+        )
+      }
+      getComponentContainer() {
+        return this.cell.prop('useForeignObject') === false
+          ? (this.selectors.content as SVGElement)
+          : (this.selectors.foContent as HTMLDivElement)
+      }
+      confirmUpdate(flag: any) {
+        const ret = super.confirmUpdate(flag)
+        return this.handleAction(ret, action, () => {
+          // 这里无需做任何处理，但是，没有这个函数的时候，会卡死...
+        })
+      }
     }
+    VuePortalShapeView.config({
+      bootstrap: [action],
+      actions: {
+        component: action,
+      },
+    })
+
+    NodeView.registry.register(uniqViewId, VuePortalShapeView, true)
+
+    return TeleportContainer
   }
-  VuePortalShapeView.config({
-    bootstrap: [action],
-    actions: {
-      component: action,
-    },
-  })
-
-  NodeView.registry.register(uniqViewId, VuePortalShapeView, true)
-
-  return TeleportContainer
+  // 如果是vue2就默认输出一个警告信息
+  console.warn('useTeleport should run in vue3')
+  // 或者拿默认的view注册一个，保证这个api是可用状态，不至于用户使用了，但是报错
+  NodeView.registry.register(uniqViewId, VueShapeView, true)
+  return defineComponent(() => null)
 }
+
+export default useTeleport
