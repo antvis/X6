48def793edf34352fd85d04e60c5d5ca004954e3
vector <vectorse@126.com>
2022-09-27 20:06:17 +0800

    chore: 🔧 remove some apps (#2728)

--- examples/x6-app-er/.umirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'umi';
-
-export default defineConfig({
-  publicPath: './',
-  routes: [
-    { path: '/', component: '@/pages/index' },
-    { path: '/apps/er', component: '@/pages/index' },
-  ],
-});
--- examples/x6-app-er/src/pages/xflow/graph/edge.ts
+++
@@ -1,7 +0,0 @@
-import { Edge as X6Edge } from '@antv/x6';
-
-export default class Edge extends X6Edge {
-  constructor() {
-    super();
-  }
-}
