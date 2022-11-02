b00cba9b4d3ee6a1934660dc5bb82dd17ecc9293
bubkoo <bubkoo.wy@gmail.com>
2022-11-02 11:38:13 +0800

    chore: force to use pnpm

---
+++ scripts/preinstall.js
@@ -0,0 +1,6 @@
+if (!/pnpm/.test(process.env.npm_execpath || '')) {
+  console.warn(
+    `This repository requires using pnpm as the package manager for scripts to work properly.`,
+  )
+  process.exit(1)
+}
