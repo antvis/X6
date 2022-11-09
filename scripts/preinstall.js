6e7fa9d602b1506cd837bb24ebe5b9bb3f4c48fa
bubkoo <bubkoo.wy@gmail.com>
2022-11-09 14:11:05 +0800

    chore: config rss

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 if (!/pnpm/.test(process.env.npm_execpath || '')) {
   console.warn(
     `This repository requires using pnpm as the package manager for scripts to work properly.`,
