a5f7a2ad67f8ab627860800172d38ed2c3a9b603
newbyvector <vectorse@126.com>
2022-11-13 17:38:24 +0800

    docs: add docs for edge

--- sites/x6-sites-demos/packages/tutorial/basic/edge/custom-marker/src/app.tsx
+++ sites/x6-sites-demos/packages/tutorial/basic/edge/custom-marker/src/app.tsx
@@ -8,13 +8,15 @@ export default class Example extends React.Component {
   componentDidMount() {
     const graph = new Graph({
       container: this.container,
-      grid: true,
+      background: {
+        color: '#F2F7FA',
+      },
     })
 
-    const edge = graph.addEdge({
-      source: [228.84550125020417, 100.76702664502545],
-      target: [416.2834258874138, 72.03741369165368],
-      vertices: [{ x: 300, y: 150 }],
+    graph.addEdge({
+      source: [200, 140],
+      target: [500, 140],
+      label: 'custom-marker',
       attrs: {
         line: {
           sourceMarker: {
@@ -23,70 +25,15 @@ export default class Example extends React.Component {
           },
           targetMarker: {
             tagName: 'path',
-            stroke: 'green',
+            stroke: '#D94111',
             strokeWidth: 2,
-            fill: 'yellow',
+            fill: '#90C54C',
             d: 'M 20 -10 0 0 20 10 Z',
           },
+          stroke: '#8f8f8f',
+          strokeWidth: 1,
         },
       },
-    })
-
-    function hourHand() {
-      edge.transition('source', (10 + 9.36 / 60) / 12, {
-        delay: 1000,
-        duration: 19000,
-        interp: (start, startTime) => {
-          const timeCorrection = startTime * (2 * Math.PI) - Math.PI / 2
-          const origin = { x: 300, y: 150 }
-          const radius = 140 / 1.618
-          return function (t) {
-            return {
-              x: origin.x + radius * Math.cos(t * 2 * Math.PI + timeCorrection),
-              y: origin.y + radius * Math.sin(t * 2 * Math.PI + timeCorrection),
-            }
-          }
-        },
-      })
-    }
-
-    function minuteHand() {
-      edge.transition('target', 9.36 / 60, {
-        delay: 1000,
-        duration: 19000,
-        timing: (time) => {
-          return time * 12 - Math.floor(time * 12)
-        },
-        interp: (start, startTime) => {
-          const timeCorrection = startTime * (2 * Math.PI) - Math.PI / 2
-          const origin = { x: 300, y: 150 }
-          const radius = 140
-          return function (t) {
-            return {
-              x: origin.x + radius * Math.cos(t * 2 * Math.PI + timeCorrection),
-              y: origin.y + radius * Math.sin(t * 2 * Math.PI + timeCorrection),
-            }
-          }
-        },
-      })
-    }
-
-    let currentTransitions = 0
-
-    hourHand()
-    minuteHand()
-
-    edge.on('transition:start', () => {
-      currentTransitions += 1
-    })
-
-    edge.on('transition:complete', () => {
-      currentTransitions -= 1
-
-      if (currentTransitions === 0) {
-        hourHand()
-        minuteHand()
-      }
     })
   }
 
--- sites/x6-sites-demos/packages/tutorial/basic/edge/router/src/app.tsx
+++ sites/x6-sites-demos/packages/tutorial/basic/edge/router/src/app.tsx
@@ -2,42 +2,86 @@ import React from 'react'
 import { Graph } from '@antv/x6'
 import './app.css'
 
+Graph.registerNode(
+  'custom-node',
+  {
+    inherit: 'rect', // 继承于 rect 节点
+    width: 100,
+    height: 40,
+    markup: [
+      {
+        tagName: 'rect', // 标签名称
+        selector: 'body', // 选择器
+      },
+      {
+        tagName: 'image',
+        selector: 'img',
+      },
+      {
+        tagName: 'text',
+        selector: 'label',
+      },
+    ],
+    attrs: {
+      body: {
+        stroke: '#8f8f8f',
+        strokeWidth: 1,
+        fill: '#fff',
+        rx: 6,
+        ry: 6,
+      },
+      img: {
+        'xlink:href':
+          'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png',
+        width: 16,
+        height: 16,
+        x: 12,
+        y: 12,
+      },
+    },
+  },
+  true,
+)
+
 export default class Example extends React.Component {
   private container: HTMLDivElement
 
   componentDidMount() {
     const graph = new Graph({
       container: this.container,
-      grid: true,
+      background: {
+        color: '#F2F7FA',
+      },
     })
 
-    const rect1 = graph.addNode({
-      x: 30,
-      y: 30,
-      width: 100,
-      height: 40,
+    const source = graph.addNode({
+      shape: 'custom-node', // 可以直接使用上面注册过的 shape
+      x: 40,
+      y: 40,
       label: 'hello',
     })
 
-    const rect2 = graph.addNode({
+    const target = graph.addNode({
+      shape: 'custom-node',
       x: 300,
-      y: 240,
-      width: 100,
-      height: 40,
+      y: 220,
       label: 'world',
     })
 
     graph.addEdge({
-      source: rect1,
-      target: rect2,
+      source,
+      target,
+      attrs: {
+        line: {
+          stroke: '#8f8f8f',
+          strokeWidth: 1,
+        },
+      },
       vertices: [
         { x: 100, y: 200 },
         { x: 300, y: 120 },
       ],
-      router: {
-        name: 'orth',
-        args: { padding: 50 },
-      },
+      router: 'orth',
     })
   }
 
