547bb7823b93357be5c144f82cad10462d441f6c
newbyvector <vectorse@126.com>
2022-11-14 21:22:21 +0800

    docs: add demos for react render

---
+++ sites/x6-sites-demos/packages/tutorial/intermediate/react/update/src/app.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Graph, Node } from '@antv/x6'
+import { register } from '@antv/x6-react-shape'
+import { Progress } from 'antd'
+import './app.css'
+
+const NodeComponent: any = ({ node }: { node: Node }) => {
+  const { progress } = node.getData()
+  return (
+    <div className="react-node">
+      <Progress type="circle" percent={progress} width={80} />
+    </div>
+  )
+}
+
+register({
+  shape: 'custom-react-node',
+  width: 100,
+  height: 100,
+  effect: ['data'],
+  component: NodeComponent,
+})
+
+export default class Example extends React.Component {
+  private container: HTMLDivElement
+
+  componentDidMount() {
+    const graph = new Graph({
+      container: this.container,
+      background: {
+        color: '#F2F7FA',
+      },
+    })
+
+    const node = graph.addNode({
+      shape: 'custom-react-node',
+      x: 60,
+      y: 100,
+      data: {
+        progress: 30,
+      },
+    })
+
+    setInterval(() => {
+      const { progress } = node.getData<{ progress: number }>()
+      node.setData({
+        progress: (progress + 10) % 100,
+      })
+    }, 1000)
+
+    graph.centerContent()
+  }
+
+  refContainer = (container: HTMLDivElement) => {
+    this.container = container
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="app-content" ref={this.refContainer} />
+      </div>
+    )
+  }
+}
