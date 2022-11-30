f920d5a4ee4626fc3dbf252246a49cefe74a84e9
vector <vectorse@126.com>
2022-11-30 10:50:30 +0800

    fix: export toJpeg methods in export plugin (#2974)

--- packages/x6-plugin-export/src/api.ts
+++ packages/x6-plugin-export/src/api.ts
@@ -3,9 +3,51 @@ import { Export } from './index'
 
 declare module '@antv/x6/lib/graph/graph' {
   interface Graph {
+    toSVG: (
+      callback: Export.ToSVGCallback,
+      options: Export.ToSVGOptions,
+    ) => void
+    toPNG: (
+      callback: Export.ToSVGCallback,
+      options: Export.ToImageOptions,
+    ) => void
+    toJPEG: (
+      callback: Export.ToSVGCallback,
+      options: Export.ToImageOptions,
+    ) => void
     exportPNG: (fileName?: string, options?: Export.ToImageOptions) => void
     exportJPEG: (fileName?: string, options?: Export.ToImageOptions) => void
     exportSVG: (fileName?: string, options?: Export.ToSVGOptions) => void
+  }
+}
+
+Graph.prototype.toSVG = function (
+  callback: Export.ToSVGCallback,
+  options: Export.ToSVGOptions,
+) {
+  const instance = this.getPlugin('export') as Export
+  if (instance) {
+    instance.toSVG(callback, options)
+  }
+}
+
+Graph.prototype.toPNG = function (
+  callback: Export.ToSVGCallback,
+  options: Export.ToImageOptions,
+) {
+  const instance = this.getPlugin('export') as Export
+  if (instance) {
+    instance.toPNG(callback, options)
+  }
+}
+
+Graph.prototype.toJPEG = function (
+  callback: Export.ToSVGCallback,
+  options: Export.ToImageOptions,
+) {
+  const instance = this.getPlugin('export') as Export
+  if (instance) {
+    instance.toJPEG(callback, options)
   }
 }
 
