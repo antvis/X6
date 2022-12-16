176cb4eef9c263b19f964a2de4eae86ca93d1d03
njshuisheng <njshuisheng@126.com>
2022-12-16 11:19:42 +0800

    fix: :bug: grammar style improvement

--- packages/x6/src/registry/tool/editor.ts
+++ packages/x6/src/registry/tool/editor.ts
@@ -95,13 +95,7 @@ export class CellEditor extends ToolsView.ToolItem<
     editor.innerText = text || ''
 
     // clear display value when edit status because char ghosting.
-    const setText = this.options.setText
-    FunctionExt.call(setText, this.cellView, {
-      cell: this.cell,
-      value: '',
-      index: this.labelIndex,
-      distance: this.distance,
-    })
+    this.setCellText('')
 
     return this
   }
@@ -111,15 +105,7 @@ export class CellEditor extends ToolsView.ToolItem<
       const cell = this.cell
       const value = this.editor.innerText.replace(/\n$/, '') || ''
       // set value
-      const setText = this.options.setText
-      if (typeof setText === 'function') {
-        FunctionExt.call(setText, this.cellView, {
-          cell: this.cell,
-          value,
-          index: this.labelIndex,
-          distance: this.distance,
-        })
-      }
+      this.setCellText(value)
       // remove tool
       cell.removeTool(cell.isEdge() ? 'edge-editor' : 'node-editor')
       this.undelegateDocumentEvents()
@@ -148,6 +134,18 @@ export class CellEditor extends ToolsView.ToolItem<
       range.selectNodeContents(this.editor)
       selection.removeAllRanges()
       selection.addRange(range)
+    }
+  }
+
+  setCellText(value: string) {
+    const setText = this.options.setText
+    if (typeof setText === 'function') {
+      FunctionExt.call(setText, this.cellView, {
+        cell: this.cell,
+        value,
+        index: this.labelIndex,
+        distance: this.distance,
+      })
     }
   }
 }
