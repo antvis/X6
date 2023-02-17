de9701e90ed34f07dfbe5ce89549dbb73abe8036
Struggle <1178825961@qq.com>
2023-02-17 21:49:58 +0800

    feat: targetview not mounted can render edge

--- packages/x6/src/view/edge.ts
+++ packages/x6/src/view/edge.ts
@@ -108,18 +108,19 @@ export class EdgeView<
       ref = this.removeAction(ref, 'target')
     }
 
-    // const graph = this.graph
-    // const sourceView = this.sourceView
+    const graph = this.graph
+    const sourceView = this.sourceView
     // const targetView = this.targetView
-    // console.log(sourceView, targetView)
-    // if (
-    //   graph &&
-    //   ((sourceView && !graph.renderer.isViewMounted(sourceView)) ||
-    //     (targetView && !graph.renderer.isViewMounted(targetView)))
-    // ) {
-    //   // Wait for the sourceView and targetView to be rendered.
-    //   return ref
-    // }
+
+    if (
+      graph &&
+      sourceView &&
+      !graph.renderer.isViewMounted(sourceView)
+      // (targetView && !graph.renderer.isViewMounted(targetView)))
+    ) {
+      // Wait for the sourceView and targetView to be rendered.
+      return ref
+    }
 
     if (this.hasAction(ref, 'render')) {
       this.render()
