631749334715d5723ae3d7c289307be198b30707
vector <vectorse@126.com>
2023-02-27 10:54:45 +0800

    docs: optimize examples style for doc (#3306)

--- sites/x6-sites/src/tutorial/basic/graph/background-grid/index.tsx
+++ sites/x6-sites/src/tutorial/basic/graph/background-grid/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Graph } from '@antv/x6'
-import styles from './index.less'
+import './index.less'
 
 const data = {
   nodes: [
@@ -93,8 +93,8 @@ export default class Example extends React.Component {
 
   render() {
     return (
-      <div className={styles.app}>
-        <div className={styles['app-content']} ref={this.refContainer} />
+      <div className="backgournd-grid-app">
+        <div className="app-content" ref={this.refContainer} />
       </div>
     )
   }
