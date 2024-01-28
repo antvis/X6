1a647d5c028b8880222e64a589b270f54bc3efaf
你捉不到的this <2228429150@qq.com>
2024-01-28 10:45:09 +0800

    fix: update xflow hooks demo

--- sites/x6-sites/src/xflow/hooks/use-graph-store/index.tsx
+++ sites/x6-sites/src/xflow/hooks/use-graph-store/index.tsx
@@ -1,48 +1,8 @@
-import { XFlow, XFlowGraph, Background, useGraphStore } from '@antv/xflow'
+import { XFlow, XFlowGraph, Background } from '@antv/xflow'
 import React from 'react'
-import { Button, Space } from 'antd'
+import { ToolsButton } from './tools-button'
+
 import './index.less'
-
-const ToolsButton = () => {
-  const addNodes = useGraphStore((state) => state.addNodes)
-  const removeNodes = useGraphStore((state) => state.removeNodes)
-
-  const onAddNodes = () => {
-    addNodes([
-      {
-        id: '3',
-        shape: 'rect',
-        x: 200,
-        y: 100,
-        width: 100,
-        height: 40,
-        attrs: {
-          body: {
-            stroke: '#8f8f8f',
-            strokeWidth: 1,
-            fill: '#fff',
-            rx: 6,
-            ry: 6,
-          },
-        },
-        label: 'added',
-      },
-    ])
-  }
-
-  const onRemoveNodes = () => {
-    removeNodes(['3'])
-  }
-
-  return (
-    <div className="xflow-hooks-use-graph-store-header">
-      <Space>
-        <Button onClick={onAddNodes}>addNode</Button>
-        <Button onClick={onRemoveNodes}>removeNode</Button>
-      </Space>
-    </div>
-  )
-}
 
 const Page = () => {
   return (
