ff4bf7121b51f8f6220897442bc9a77ccace8839
yelikang <530331059@qq.com>
2024-08-19 13:38:39 +0800

    feat: Dom公共方法封装（可借鉴）

--- packages/x6-common/src/dom/index.ts
+++ packages/x6-common/src/dom/index.ts
@@ -1,3 +1,3 @@
 import * as Dom from './main'
-
+// 封装Dom相关的操作方法，可以借鉴
 export { Dom }
--- packages/x6-common/src/dom/position.ts
+++ packages/x6-common/src/dom/position.ts
@@ -20,7 +20,11 @@ export function height(elem: Element) {
   const rect = elem.getBoundingClientRect()
   return rect.height
 }
-
+/**
+ * 计算元素相对于父元素的绝对定位
+ * @param elem 
+ * @returns 
+ */
 export function position(elem: Element) {
   const isFixed = computeStyle(elem, 'position') === 'fixed'
   let offsetValue: ReturnType<typeof offset>
@@ -38,6 +42,7 @@ export function position(elem: Element) {
       (offsetParent === doc.body || offsetParent === doc.documentElement) &&
       computeStyle(offsetParent, 'position') === 'static'
     ) {
+      // 如果 offsetParent 是 body 或 html 并且其定位属性为 static，则继续寻找更高层的定位父元素
       offsetParent = offsetParent.parentNode
     }
     if (offsetParent !== elem && isElement(offsetParent)) {
