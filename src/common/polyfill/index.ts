a4fdb17c95c9d067361080e98164a904d1190231
hustcc <i@hust.cc>
2025-09-03 17:26:11 +0800

    test: add testcase for size-sensor, polyfill

--- src/common/polyfill/index.ts
+++ src/common/polyfill/index.ts
@@ -1,3 +1,28 @@
+export function forEachPolyfill(arr) {
+  arr.forEach((item) => {
+    if (Object.hasOwn(item, 'append')) {
+      return
+    }
+    Object.defineProperty(item, 'append', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value(...args: any[]) {
+        const docFrag = document.createDocumentFragment()
+
+        args.forEach((arg: any) => {
+          const isNode = arg instanceof Node
+          docFrag.appendChild(
+            isNode ? arg : document.createTextNode(String(arg)),
+          )
+        })
+
+        this.appendChild(docFrag)
+      },
+    })
+  })
+}
+
 // compatible with NodeList.prototype.forEach() before chrome 51
 // https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
 if (
@@ -11,28 +36,7 @@ if (
 // compatible with ParentNode.append() before chrome 54
 // https://github.com/jserz/js_piece/blob/master/DOM/ParentNode/append()/append().md
 if (typeof window !== 'undefined') {
-  ;(function (arr) {
-    arr.forEach((item) => {
-      if (Object.prototype.hasOwnProperty.call(item, 'append')) {
-        return
-      }
-      Object.defineProperty(item, 'append', {
-        configurable: true,
-        enumerable: true,
-        writable: true,
-        value(...args: any[]) {
-          const docFrag = document.createDocumentFragment()
-
-          args.forEach((arg: any) => {
-            const isNode = arg instanceof Node
-            docFrag.appendChild(
-              isNode ? arg : document.createTextNode(String(arg)),
-            )
-          })
-
-          this.appendChild(docFrag)
-        },
-      })
-    })
+  ;((arr) => {
+    forEachPolyfill(arr)
   })([Element.prototype, Document.prototype, DocumentFragment.prototype])
 }
