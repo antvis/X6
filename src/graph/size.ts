9b3c4193350fd3988d58e0c271d7c1254a4d925a
桑璟 <gaofuhong.gfh@antgroup.com>
2025-09-22 15:39:47 +0800

    refactor: using ResizeObserver to improve the smoothness of resize

--- src/graph/size.ts
+++ src/graph/size.ts
@@ -1,7 +1,9 @@
-import { disposable, SizeSensor } from '../common'
+import { disposable } from '../common'
 import { Base } from './base'
 
 export class SizeManager extends Base {
+  private ro?: ResizeObserver
+
   private getScroller() {
     const scroller = this.graph.getPlugin<any>('scroller')
     if (scroller && scroller.options.enabled) {
@@ -13,16 +15,16 @@ export class SizeManager extends Base {
   private getContainer() {
     const scroller = this.getScroller()
     if (scroller) {
-      return scroller.container.parentElement
+      return scroller.container.parentElement as HTMLElement | null
     }
-    return this.graph.container.parentElement
+    return this.graph.container.parentElement as HTMLElement | null
   }
 
   private getSensorTarget() {
     const autoResize = this.options.autoResize
     if (autoResize) {
       if (typeof autoResize === 'boolean') {
-        return this.getContainer()
+        return this.getContainer() || undefined
       }
       return autoResize as HTMLElement
     }
@@ -33,11 +35,18 @@ export class SizeManager extends Base {
     if (autoResize) {
       const target = this.getSensorTarget()
       if (target) {
-        SizeSensor.bind(target, () => {
-          const width = target.offsetWidth
-          const height = target.offsetHeight
-          this.resize(width, height)
+        if (typeof ResizeObserver === 'undefined') {
+          return
+        }
+        this.ro = new ResizeObserver((entries) => {
+          if (!entries || entries.length === 0) return
+          const { width, height } = entries[0].contentRect
+          this.resize(Math.round(width), Math.round(height))
         })
+        this.ro.observe(target)
+        const width = target.offsetWidth
+        const height = target.offsetHeight
+        this.resize(width, height)
       }
     }
   }
@@ -53,6 +62,9 @@ export class SizeManager extends Base {
 
   @disposable()
   dispose() {
-    SizeSensor.clear(this.graph.container)
+    if (this.ro) {
+      this.ro.disconnect()
+      this.ro = undefined
+    }
   }
 }
