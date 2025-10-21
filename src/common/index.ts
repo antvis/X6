e4cec0e740f5f0dbde36de9b1fdd4d3350cede86
heshenglei <hsl01386768@antgroup.com>
2025-10-21 14:47:26 +0800

    refactor:  replace `export * as` with import/export for bundler compatibility

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,16 +1,18 @@
 import './polyfill'
+import * as ArrayExt from './array'
+import * as Color from './color'
+import * as CssLoader from './css-loader'
+import * as DataUri from './datauri'
+import * as FunctionExt from './function'
+import * as Unit from './unit'
+import * as Util from './util'
 
 export * from './algorithm'
 export * from './animation'
-export * as ArrayExt from './array'
 export * from './base'
-export * as Color from './color'
-export * as CssLoader from './css-loader'
-export * as DataUri from './datauri'
 export * from './dictionary'
 export * from './dom'
 export * from './event'
-export * as FunctionExt from './function'
 export {
   isModifierKeyEqual,
   isModifierKeyMatch,
@@ -23,6 +25,6 @@ export * from './platform'
 export * from './string'
 export * from './text'
 export * from './types'
-export * as Unit from './unit'
-export * as Util from './util'
 export * from './vector'
+
+export { ArrayExt, Color, CssLoader, DataUri, FunctionExt, Unit, Util }
