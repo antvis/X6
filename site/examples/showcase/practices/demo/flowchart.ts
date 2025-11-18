5c0cba6473670397ddc581825fddc4a27064efa3
ly01379069 <ly01379069@antgroup.com>
2025-11-18 17:58:47 +0800

    feat:恢复默认collapsable为true

--- site/examples/showcase/practices/demo/flowchart.ts
+++ site/examples/showcase/practices/demo/flowchart.ts
@@ -100,7 +100,7 @@ const stencil = new Stencil({
   stencilGraphWidth: 200,
   stencilGraphHeight: 180,
   stencilGraphOptions: { panning: true },
-  collapsable: false,
+  collapsable: true,
   groups: [
     {
       title: '基础流程图',
